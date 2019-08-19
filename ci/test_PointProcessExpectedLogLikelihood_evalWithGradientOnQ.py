728b47cf44bbb082a82c458bcc547cab94ced8ee
Joaquin Rapela <joacorapela@gmail.com>
2019-08-19 17:31:44 +0100

    renamed

--- ci/test_PointProcessExpectedLogLikelihood_evalWithGradientOnQ.py
+++
@@ -1,4 +0,0 @@
-
-def test_PointProcessExpectedLogLikelihood_evalWithGradientOnQ():
-    
-    ppELL = PointProcessExpectedLogLikelihood(data, legQuadPoints, legQuadWeights, hermQuadPoints, hermQuadWeights, spikeTimes):
