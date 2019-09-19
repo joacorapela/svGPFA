745c8b01214723cbf82b0e73ba88d55ea96ab480
Joaquin Rapela <joacorapela@gmail.com>
2019-09-19 19:23:52 +0100

    modified clock in src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+
+import pdb
+import time
 import torch
 
 def getDiagIndicesIn3DArray(N, M):
@@ -30,12 +33,28 @@ def pinv3D(K):
     return Kinv
 
 def clock(func):
-    def clocked(*args):
+    def clocked(*args,**kargs):
         t0 = time.perf_counter()
-        result = func(*args)
+        result = func(*args,**kargs)
         elapsed = time.perf_counter()-t0
         name = func.__name__
-        arg_str = ', '.join(repr(arg) for arg in args)
-        print('[%.0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        if len(args)>0:
+            arg_str = ', '.join(repr(arg) for arg in args)
+        else:
+            arg_str = None
+        if len(kargs)>0:
+            keys = kargs.keys()
+            values = kargs.values()
+            karg_str = ', '.join(key + "=" + repr(value) for key in keys for value in values)
+        else:
+            karg_str = None
+        if arg_str is not None and karg_str is not None:
+            print('[%0.8fs] %s(%s,%s) -> %r' % (elapsed, name, arg_str, karg_str, result))
+        elif arg_str is not None:
+            print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        elif karg_str is not None:
+            print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, karg_str, result))
+        else:
+            print('[%0.8fs] %s() -> %r' % (elapsed, name, result))
         return result
     return clocked
