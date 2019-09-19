3a35ee154eea438513ddb0271845c52aff7d42d3
Joaquin Rapela <joacorapela@gmail.com>
2019-09-19 08:53:57 +0100

    moved test_buildQSigma to ci/test_inducingPointsPrior.py

--- ci/test_klDivergence.py
+++ ci/test_klDivergence.py
@@ -11,23 +11,6 @@ from kernelMatricesStore import KernelMatricesStore
 from klDivergence import KLDivergence
 from inducingPointsPrior import InducingPointsPrior
 
-def test_buildQSigma():
-    tol = 1e-5
-    dataFilename = os.path.expanduser('data/get_full_from_lowplusdiag.mat')
-
-    mat = loadmat(dataFilename)
-    nLatents = mat['q_sqrt'].shape[1]
-    q_sqrt = [torch.from_numpy(mat['q_sqrt'][(0,i)]).permute(2,0,1) for i in range(nLatents)]
-    q_diag = [torch.from_numpy(mat['q_diag'][(0,i)]).permute(2,0,1) for i in range(nLatents)]
-    q_sigma = [torch.from_numpy(mat['q_sigma'][(0,i)]).permute(2,0,1) for i in range(nLatents)]
-
-    svLowerBound = SparseVariationalLowerBound(eLL=None, covMatricesStore=None, qMu=None, qSVec=None, qSDiag=None, C=None, d=None, kernelParams=None, varRnk=torch.ones(3, dtype=torch.uint8), neuronForSpikeIndex=None)
-    qSigma = svLowerBound._SparseVariationalLowerBound__buildQSigma(qSVec=q_sqrt, qSDiag=q_diag)
-
-    error = torch.tensor([(qSigma[k]-q_sigma[k]).norm() for k in range(len(qSigma))]).sum()
-
-    assert(error<tol)
-
 def test_evalSumAcrossLatentsTrials():
     tol = 1e-5
     dataFilename = os.path.expanduser("data/Estep_Objective_PointProcess_svGPFA.mat")
