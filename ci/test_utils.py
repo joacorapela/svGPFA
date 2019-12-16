fe8241e1e19e8644c7af7ecce7622c963cf4cf6b
Joaquin Rapela <joacorapela@gmail.com>
2019-12-16 08:58:58 +0000

    Commented test_j_cholesky

--- ci/test_utils.py
+++ ci/test_utils.py
@@ -24,20 +24,20 @@ def test_build3DdiagFromDiagVector():
     trueD = torch.tensor([[[0,0,0],[0,1,0],[0,0,2]],[[3,0,0],[0,4,0],[0,0,5]]], dtype=torch.double)
     assert(((trueD-D)**2).sum()==0)
 
-def test_j_cholesky():
-    tol = 1e-3
-
-    A = torch.randn((3, 4))
-    K = torch.mm(A, A.T)
-    trueY = torch.unsqueeze(torch.tensor([1.0, 2.0, 3.0]), 1)
-    b = torch.mm(K, trueY)
-    KChol = torch.cholesky(K)
-    yTorch = torch.cholesky_solve(b, KChol)
-    yJ = stats.svGPFA.utils.j_cholesky_solve(b, KChol)
-    error = ((yTorch-yJ)**2).sum()
-    assert(error<tol)
-
+# def test_j_cholesky():
+#     tol = 1e-3
+# 
+#     A = torch.randn((3, 4))
+#     K = torch.mm(A, A.T)
+#     trueY = torch.unsqueeze(torch.tensor([1.0, 2.0, 3.0]), 1)
+#     b = torch.mm(K, trueY)
+#     KChol = torch.cholesky(K)
+#     yTorch = torch.cholesky_solve(b, KChol)
+#     yJ = stats.svGPFA.utils.j_cholesky_solve(b, KChol)
+#     error = ((yTorch-yJ)**2).sum()
+#     assert(error<tol)
+# 
 if __name__=="__main__":
-    # test_getDiagIndicesIn3DArray()
-    # test_build3DdiagFromDiagVector()
+    test_getDiagIndicesIn3DArray()
+    test_build3DdiagFromDiagVector()
     # test_j_cholesky()
