f596abb373575f1fd205baea9e833aa969117aaf
Joaquin Rapela <joacorapela@gmail.com>
2019-12-16 07:59:48 +0000

    Changed format of YNonStacked

--- ci/test_svLowerBound.py
+++ ci/test_svLowerBound.py
@@ -25,9 +25,6 @@ def test_eval_pointProcess():
     yNonStackedFilename = os.path.join(os.path.dirname(__file__), "data/YNonStacked.mat")
     dataFilename = os.path.join(os.path.dirname(__file__), "data/Estep_Objective_PointProcess_svGPFA.mat")
 
-    mat = loadmat(yNonStackedFilename)
-    YNonStacked = mat['YNonStacked']
-
     mat = loadmat(dataFilename)
     nLatents = len(mat['Z'])
     nTrials = mat['Z'][0,0].shape[2]
@@ -43,6 +40,14 @@ def test_eval_pointProcess():
     kernelNames = mat["kernelNames"]
     hprs = mat["hprs"]
 
+    yMat = loadmat(yNonStackedFilename)
+    YNonStacked_tmp = yMat['YNonStacked']
+    nNeurons = YNonStacked_tmp[0,0].shape[0]
+    YNonStacked = [[[] for n in range(nNeurons)] for r in range(nTrials)]
+    for r in range(nTrials):
+        for n in range(nNeurons):
+            YNonStacked[r][n] = YNonStacked_tmp[r,0][n,0][:,0]
+
     linkFunction = torch.exp
 
     kernels = [[None] for k in range(nLatents)]
@@ -50,8 +55,8 @@ def test_eval_pointProcess():
     for k in range(nLatents):
         if np.char.equal(kernelNames[0,k][0], "PeriodicKernel"):
             kernels[k] = PeriodicKernel(scale=1.0)
-            kernelsParams0[k] = torch.tensor([float(hprs[k,0][0]), 
-                                              float(hprs[k,0][1])], 
+            kernelsParams0[k] = torch.tensor([float(hprs[k,0][0]),
+                                              float(hprs[k,0][1])],
                                              dtype=torch.double)
         elif np.char.equal(kernelNames[0,k][0], "rbfKernel"):
             kernels[k] = ExponentialQuadraticKernel(scale=1.0)
@@ -65,19 +70,19 @@ def test_eval_pointProcess():
     indPointsLocsAndAllTimesKMS = IndPointsLocsAndAllTimesKMS()
     indPointsLocsAndAssocTimesKMS = IndPointsLocsAndAssocTimesKMS()
     qKAllTimes = SVPosteriorOnLatentsAllTimes(
-        svPosteriorOnIndPoints=qU, 
-        indPointsLocsKMS=indPointsLocsKMS, 
+        svPosteriorOnIndPoints=qU,
+        indPointsLocsKMS=indPointsLocsKMS,
         indPointsLocsAndTimesKMS=indPointsLocsAndAllTimesKMS)
     qKAssocTimes = SVPosteriorOnLatentsAssocTimes(
-        svPosteriorOnIndPoints=qU, 
-        indPointsLocsKMS=indPointsLocsKMS, 
+        svPosteriorOnIndPoints=qU,
+        indPointsLocsKMS=indPointsLocsKMS,
         indPointsLocsAndTimesKMS=indPointsLocsAndAssocTimesKMS)
     qHAllTimes = LinearSVEmbeddingAllTimes(svPosteriorOnLatents=qKAllTimes)
     qHAssocTimes = LinearSVEmbeddingAssocTimes(svPosteriorOnLatents=
                                                qKAssocTimes)
     eLL = PointProcessELLExpLink(svEmbeddingAllTimes=qHAllTimes,
                                  svEmbeddingAssocTimes=qHAssocTimes)
-    klDiv = KLDivergence(indPointsLocsKMS=indPointsLocsKMS, 
+    klDiv = KLDivergence(indPointsLocsKMS=indPointsLocsKMS,
                          svPosteriorOnIndPoints=qU)
     svlb = SVLowerBound(eLL=eLL, klDiv=klDiv)
 
@@ -88,7 +93,7 @@ def test_eval_pointProcess():
     initialParams = {"svPosteriorOnIndPoints": qUParams0,
                      "kernelsMatricesStore": kmsParams0,
                      "svEmbedding": qHParams0}
-    quadParams = {"legQuadPoints": legQuadPoints, 
+    quadParams = {"legQuadPoints": legQuadPoints,
                   "legQuadWeights": legQuadWeights}
 
     svlb.setKernels(kernels=kernels)
@@ -103,55 +108,53 @@ def test_eval_pointProcess():
 
     # pdb.set_trace()
 
-'''
-def test_eval_poisson():
-    tol = 1e-5
-    dataFilename = os.path.join(os.path.dirname(__file__), "data/Estep_Objective_svGPFA.mat")
-
-    mat = loadmat(dataFilename)
-    nLatents = len(mat['q_mu'])
-    nTrials = mat['q_mu'][0,0].shape[2]
-    qMu = [torch.from_numpy(mat['q_mu'][(i,0)]).type(torch.DoubleTensor).permute(2,0,1) for i in range(nLatents)]
-    qSVec = [torch.from_numpy(mat['q_sqrt'][(i,0)]).type(torch.DoubleTensor).permute(2,0,1) for i in range(nLatents)]
-    qSDiag = [torch.from_numpy(mat['q_diag'][(i,0)]).type(torch.DoubleTensor).permute(2,0,1) for i in range(nLatents)]
-    t_tmp = torch.from_numpy(mat['tt']).type(torch.DoubleTensor).squeeze()
-    Z = [torch.from_numpy(mat['Z'][(i,0)]).type(torch.DoubleTensor).permute(2,0,1) for i in range(nLatents)]
-    Y = torch.from_numpy(mat['Y']).type(torch.DoubleTensor).permute(2,0,1) 
-    C = torch.from_numpy(mat["C"]).type(torch.DoubleTensor)
-    b = torch.from_numpy(mat["b"]).type(torch.DoubleTensor).squeeze()
-    hermQuadPoints = torch.from_numpy(mat['xxHerm']).type(torch.DoubleTensor)
-    hermQuadWeights = torch.from_numpy(mat['wwHerm']).type(torch.DoubleTensor)
-    binWidth = torch.from_numpy(mat['BinWidth'])
-    obj = mat['obj'][0,0]
-
-    # t_tmp \in nQuad and we want t \in nTrials x nQuad x 1
-    t = torch.ger(input=torch.ones(nTrials, dtype=torch.double), vec2=t_tmp).unsqueeze(dim=2)
-
-    linkFunction = torch.exp
-
-    kernelNames = mat["kernelNames"]
-    hprs = mat["hprs"]
-    kernels = [[None] for k in range(nLatents)]
-    for k in range(nLatents):
-        if np.char.equal(kernelNames[0,k][0], 'PeriodicKernel'):
-            kernels[k] = PeriodicKernel(scale=1.0, lengthScale=float(hprs[k,0][0]), period=float(hprs[k,0][1]))
-        elif np.char.equal(kernelNames[0,k][0], 'rbfKernel'):
-            kernels[k] = ExponentialQuadraticKernel(scale=1.0, lengthScale=float(hprs[k,0][0]))
-        else:
-            raise ValueError("Invalid kernel name: %s"%(kernelNames[k]))
-
-    qU = InducingPointsPrior(qMu=qMu, qSVec=qSVec, qSDiag=qSDiag, varRnk=torch.ones(3,dtype=torch.uint8))
-    kernelMatricesStore = KernelMatricesStore(kernels=kernels, Z=Z, t=t, Y=Y)
-    qH = ApproxPosteriorForHForAllNeuronsAllTimes(C=C, d=b, inducingPointsPrior=qU, kernelMatricesStore=kernelMatricesStore)
-
-    eLL = PoissonExpectedLogLikelihood(approxPosteriorForHForAllNeuronsAllTimes=qH, hermQuadPoints=hermQuadPoints, hermQuadWeights=hermQuadWeights, linkFunction=linkFunction, Y=Y, binWidth=binWidth)
-    klDiv = KLDivergence(kernelMatricesStore=kernelMatricesStore, inducingPointsPrior=qU)
-    svlb = SparseVariationalLowerBound(eLL=eLL, klDiv=klDiv)
-    lbEval = svlb.eval()
-
-    assert(abs(lbEval+obj)<tol)
-'''
-    
+# def test_eval_poisson():
+#     tol = 1e-5
+#     dataFilename = os.path.join(os.path.dirname(__file__), "data/Estep_Objective_svGPFA.mat")
+# 
+#     mat = loadmat(dataFilename)
+#     nLatents = len(mat['q_mu'])
+#     nTrials = mat['q_mu'][0,0].shape[2]
+#     qMu = [torch.from_numpy(mat['q_mu'][(i,0)]).type(torch.DoubleTensor).permute(2,0,1) for i in range(nLatents)]
+#     qSVec = [torch.from_numpy(mat['q_sqrt'][(i,0)]).type(torch.DoubleTensor).permute(2,0,1) for i in range(nLatents)]
+#     qSDiag = [torch.from_numpy(mat['q_diag'][(i,0)]).type(torch.DoubleTensor).permute(2,0,1) for i in range(nLatents)]
+#     t_tmp = torch.from_numpy(mat['tt']).type(torch.DoubleTensor).squeeze()
+#     Z = [torch.from_numpy(mat['Z'][(i,0)]).type(torch.DoubleTensor).permute(2,0,1) for i in range(nLatents)]
+#     Y = torch.from_numpy(mat['Y']).type(torch.DoubleTensor).permute(2,0,1)
+#     C = torch.from_numpy(mat["C"]).type(torch.DoubleTensor)
+#     b = torch.from_numpy(mat["b"]).type(torch.DoubleTensor).squeeze()
+#     hermQuadPoints = torch.from_numpy(mat['xxHerm']).type(torch.DoubleTensor)
+#     hermQuadWeights = torch.from_numpy(mat['wwHerm']).type(torch.DoubleTensor)
+#     binWidth = torch.from_numpy(mat['BinWidth'])
+#     obj = mat['obj'][0,0]
+# 
+#     # t_tmp \in nQuad and we want t \in nTrials x nQuad x 1
+#     t = torch.ger(input=torch.ones(nTrials, dtype=torch.double), vec2=t_tmp).unsqueeze(dim=2)
+# 
+#     linkFunction = torch.exp
+# 
+#     kernelNames = mat["kernelNames"]
+#     hprs = mat["hprs"]
+#     kernels = [[None] for k in range(nLatents)]
+#     for k in range(nLatents):
+#         if np.char.equal(kernelNames[0,k][0], 'PeriodicKernel'):
+#             kernels[k] = PeriodicKernel(scale=1.0, lengthScale=float(hprs[k,0][0]), period=float(hprs[k,0][1]))
+#         elif np.char.equal(kernelNames[0,k][0], 'rbfKernel'):
+#             kernels[k] = ExponentialQuadraticKernel(scale=1.0, lengthScale=float(hprs[k,0][0]))
+#         else:
+#             raise ValueError("Invalid kernel name: %s"%(kernelNames[k]))
+# 
+#     qU = InducingPointsPrior(qMu=qMu, qSVec=qSVec, qSDiag=qSDiag, varRnk=torch.ones(3,dtype=torch.uint8))
+#     kernelMatricesStore = KernelMatricesStore(kernels=kernels, Z=Z, t=t, Y=Y)
+#     qH = ApproxPosteriorForHForAllNeuronsAllTimes(C=C, d=b, inducingPointsPrior=qU, kernelMatricesStore=kernelMatricesStore)
+# 
+#     eLL = PoissonExpectedLogLikelihood(approxPosteriorForHForAllNeuronsAllTimes=qH, hermQuadPoints=hermQuadPoints, hermQuadWeights=hermQuadWeights, linkFunction=linkFunction, Y=Y, binWidth=binWidth)
+#     klDiv = KLDivergence(kernelMatricesStore=kernelMatricesStore, inducingPointsPrior=qU)
+#     svlb = SparseVariationalLowerBound(eLL=eLL, klDiv=klDiv)
+#     lbEval = svlb.eval()
+# 
+#     assert(abs(lbEval+obj)<tol)
+# 
 if __name__=='__main__':
     test_eval_pointProcess()
     # test_eval_poisson()
