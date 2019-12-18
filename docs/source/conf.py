b589012c51046277d6d38df4181c339f7456d7b4
Joaquin Rapela <joacorapela@gmail.com>
2019-12-18 18:45:14 +0000

    Using autoAPI

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,9 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram', 'sphinx.ext.autosummary']
+extensions = ['autoapi.extension', 'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram', 'sphinx.ext.autosummary']
+
+autoapi_dirs = ['../../src/']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
