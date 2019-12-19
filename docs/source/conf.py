68955c48fe819a2f75b2b72f0470e902201c9189
Joaquin Rapela <joacorapela@gmail.com>
2019-12-19 18:21:27 +0000

    Changed the sources location to ../../src

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath('../../src/'))
 # sys.path.insert(0, '/nfs/nhome/live/rapela/dev/research/gatsby-swc/gatsby/svGPFA/master/src/')
 
 
