7638f14649d8ba48c57e934c7c4fb4280efab5e6
Joaquin Rapela <joacorapela@gmail.com>
2019-12-19 18:14:37 +0000

    Changed absolute to relative path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('../src/'))
-sys.path.insert(0, '/nfs/nhome/live/rapela/dev/research/gatsby-swc/gatsby/svGPFA/master/src/')
+sys.path.insert(0, os.path.abspath('../src/'))
+# sys.path.insert(0, '/nfs/nhome/live/rapela/dev/research/gatsby-swc/gatsby/svGPFA/master/src/')
 
 
 # -- Project information -----------------------------------------------------
