ced05ed9fb2b4e5c09d25c26e321fa67b5721c59
Joaquin Rapela <joacorapela@gmail.com>
2019-12-19 15:42:18 +0000

    Dicarded autoAPI, went back to autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,11 +31,11 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# extensions = ['autoapi.extension', 'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram', 'sphinx.ext.autosummary']
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.inheritance_diagram', 'autoapi.extension']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram', 'sphinx.ext.autosummary']
+# extensions = ['sphinx.ext.autodoc', 'sphinx.ext.inheritance_diagram', 'autoapi.extension']
 
-autoapi_type = 'python'
-autoapi_dirs = ['../../src/']
+# autoapi_type = 'python'
+# autoapi_dirs = ['../../src/']
 
 # Added this line because when building the project in readthedocs I get
 # an error contents.rst not found
