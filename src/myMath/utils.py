5cf220a4044b847c51bc0261dec8724359ada54b
Joaquin Rapela <joacorapela@gmail.com>
2020-03-10 22:59:02 +0000

    Returning torch tensors

--- src/myMath/utils.py
+++ src/myMath/utils.py
@@ -1,11 +1,15 @@
+import pdb
+import torch
 from numpy.polynomial.legendre import leggauss
 
-def leggaussVarLimits(n, a, b):
+def leggaussVarLimits(n, a, b, dtype=torch.double):
     """
     Computers n weights and points for Gauss-Legendre numerical integration of a
     function in the interval (a,b).
     """
     x, w = leggauss(deg=n)
+    x = torch.from_numpy(x)
+    w = torch.from_numpy(w)
     xVarLimits = (x*(b-a)+(b+a))/2
     wVarLimits = (b-a)/2*w
     return xVarLimits, wVarLimits
