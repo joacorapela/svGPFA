b5f02ea5fc421c68e514b3a19cfbe3cec589056c
Joaquin Rapela <joacorapela@gmail.com>
2020-03-13 18:54:39 +0000

    Added KSTestTimeRescaling

--- src/stats/pointProcess/tests.py
+++ src/stats/pointProcess/tests.py
@@ -1,9 +1,28 @@
+
+import pdb
 import math
 import random
 import torch
 
 # function [xks,rst,rstsort,cb,n] = KS_test_time_rescaling(Y,pk)
 def timeRescaling(Y, pk, eps=1e-10):
+    pk = torch.max(pk, torch.tensor([eps]))
+    qk = -torch.log(1-pk)
+    # make the rescaled times
+    n = int(torch.sum(Y).item())
+    rISIs = torch.zeros(n-1)
+    spikeindicies = Y.nonzero()
+    for r in range(n-1):
+        total = 0
+        ind1 = spikeindicies[r].item()
+        ind2 = spikeindicies[r+1].item()
+        total = total+torch.sum(qk[ind1+1:ind2-1]).item()
+        delta = -(1.0/qk[ind2].item())*math.log(1-random.random()*(1-math.exp(-qk[ind2].item())))
+        total = total + qk[ind2].item()*delta
+        rISIs[r] = total
+    return rISIs
+
+def KSTestTimeRescaling(Y, pk, eps=1e-10):
     '''
     Y: binary sequence for the discrete time point process
     pk: event probability in each time been based on a conditional intensity function
@@ -17,21 +36,13 @@ def timeRescaling(Y, pk, eps=1e-10):
     Use DT Correction for Time Rescaling Theorem - Haslinger, Pipa and Brown (2010)
     discrete time conditional probabilities "pk"  where 0<=pk<= 1
     '''
-    pk = max(pk, eps)
-    qk = -torch.log(1-pk)
-    # make the rescaled times
-    n = torch.sum(Y)
-    rst = torch.zeros(n-1, 1)
-    spikeindicies = Y.nonzero()
-    for r in range(n-1):
-        total = 0
-        ind1 = spikeindicies[r]
-        ind2 = spikeindicies(r+1)
-        total = total+torch.sum(qk[ind1+1:ind2-1])
-        delta = -(1.0/qk(ind2))*math.log(1-random.random()*(1-math.exp(-qk[ind2])))
-        total = total + qk[ind2]*delta
-        rst[r] = total
-    # inrst=1/(n-1);
-    # xks=(0.5*inrst:inrst:1-0.5*inrst)';
-    # cb=1.36*sqrt(inrst)
-    return rst
+    rISIs = timeRescaling(Y=Y, pk=pk, eps=eps) # rescaled inter-spike intervals
+    utRISIs = 1-torch.exp(-rISIs)              # uniform-transformed rescaled inter-spike intervals
+    sUTRISIs, indices = torch.sort(utRISIs)             # sorted uniform-transformed rescaled inter-spike intervals
+    n = len(rISIs)
+    # dt = 1/(n-1)
+    # xks = torch.linspace(0.5*dt, 1-0.5*dt, 1.0/dt)
+    k = torch.linspace(start=1, end=n, steps=n)
+    uCDF = (k-0.5)/n                            # uniform cummulative distribution function
+    cb = 1.36/math.sqrt(n)                      # 95% confidence bounds for large sample sizes (see Brown et al., 2001)
+    return sUTRISIs, uCDF, cb
