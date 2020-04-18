3518c4a0c432a8083f89146af5c7cea157535029
Joaquin Rapela <joacorapela@gmail.com>
2020-04-18 17:02:09 +0100

    Moved plot routines to plotUtils

--- scripts/demoPointProcess.py
+++ scripts/demoPointProcess.py
@@ -2,61 +2,22 @@ import sys
 import os
 import pdb
 import random
-from scipy.io import loadmat
 import torch
-import numpy as np
 import pickle
 import configparser
 import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.tsa.stattools
 sys.path.append(os.path.expanduser("../src"))
+import demoUtils
 import stats.kernels
 import stats.svGPFA.svGPFAModelFactory
 import stats.svGPFA.svEM
 import plot.svGPFA.plotUtils
 import myMath.utils
-from utils.svGPFA.configUtils import getKernels, getLatentsMeansFuncs, getLinearEmbeddingParams
-from stats.pointProcess.tests import KSTestTimeRescalingNumericalCorrection
-
-def getLegQuadPointsAndWeights(nQuad, trialsLengths, dtype=torch.double):
-    nTrials = len(trialsLengths)
-    legQuadPoints = torch.empty((nTrials, nQuad, 1), dtype=dtype)
-    legQuadWeights = torch.empty((nTrials, nQuad, 1), dtype=dtype)
-    for r in range(nTrials):
-        legQuadPoints[r,:,0], legQuadWeights[r,:,0] = myMath.utils.leggaussVarLimits(n=nQuad, a=0, b=trialsLengths[r])
-    return legQuadPoints, legQuadWeights
-
-def getIndPointLocs0(nIndPointsPerLatent, trialsLengths, firstIndPoint):
-    nLatents = len(nIndPointsPerLatent)
-    nTrials = len(trialsLengths)
-
-    Z0 = [None]*nLatents
-    for i in range(nLatents):
-        Z0[i] = torch.empty((nTrials, nIndPointsPerLatent[i], 1), dtype=torch.double)
-    for i in range(nLatents):
-        for j in range(nTrials):
-            Z0[i][j,:,0] = torch.linspace(firstIndPoint, trialsLengths[j], nIndPointsPerLatent[i])
-    return Z0
-
-def getSVPosteriorOnIndPointsParams0(nIndPointsPerLatent, nLatents, nTrials, scale):
-    qMu0 = [None]*nLatents
-    qSVec0 = [None]*nLatents
-    qSDiag0 = [None]*nLatents
-    for i in range(nLatents):
-        qMu0[i] = torch.zeros(nTrials, nIndPointsPerLatent[i], 1, dtype=torch.double)
-        qSVec0[i] = scale*torch.eye(nIndPointsPerLatent[i], 1, dtype=torch.double).repeat(nTrials, 1, 1)
-        qSDiag0[i] = scale*torch.ones(nIndPointsPerLatent[i], 1, dtype=torch.double).repeat(nTrials, 1, 1)
-    return qMu0, qSVec0, qSDiag0
-
-def getKernelsParams0(kernels, noiseSTD):
-    nTrials = len(kernels)
-    nLatents = len(kernels[0])
-    kernelsParams0 = [[] for r in range(nTrials)]
-    for r in range(nTrials):
-        kernelsParams0[r] = [[] for r in range(nLatents)]
-        for k in range(nLatents):
-            trueParams = kernels[r][k].getParams()
-            kernelsParams0[r][k] = noiseSTD*torch.randn(len(trueParams))+trueParams
-    return kernelsParams0
+import utils.svGPFA.configUtils
+import stats.pointProcess.tests
+import utils.svGPFA.miscUtils
 
 def main(argv):
     nLatents = None
@@ -72,13 +33,13 @@ def main(argv):
         print("Usage {:s} <simulation result number> <estimation init number> <nunmber of latents>".format(argv[0]))
         return
 
-    # load data and initial values
     trialToPlot = 0
     trialToPlot = 0
     neuronToPlot = 0
     dtCIF = 1e-3
     gamma = 10
 
+    # load data and initial values
     simResConfigFilename = "results/{:08d}_simulation_metaData.ini".format(simResNumber)
     simResConfig = configparser.ConfigParser()
     simResConfig.read(simResConfigFilename)
@@ -153,21 +114,24 @@ def main(argv):
 
     testTimes = torch.linspace(0, torch.max(torch.tensor(spikesTimes[0][0])), nTestPoints)
 
-    C, d = getLinearEmbeddingParams(nNeurons=nNeurons, nLatents=nLatents, config=simInitConfig)
+
+    CFilename = config["embedding_params"]["C_filename"],
+    dFilename = config["embedding_params"]["d_filename"],
+    C, d = utils.svGPFA.configUtils.getLinearEmbeddingParams(nNeurons=nNeurons, nLatents=nLatents, CFilename=CFilename, dFilename=dFilename)
     C0 = C + torch.randn(C.shape)*initCondEmbeddingSTD
     C0 = C0[:,:nLatents]
     d0 = d + torch.randn(d.shape)*initCondEmbeddingSTD
 
-    legQuadPoints, legQuadWeights = getLegQuadPointsAndWeights(nQuad=nQuad, trialsLengths=trialsLengths)
+    legQuadPoints, legQuadWeights = demoUtils.getLegQuadPointsAndWeights(nQuad=nQuad, trialsLengths=trialsLengths)
 
-    kernels = getKernels(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
-    kernelsParams0 = getKernelsParams0(kernels=kernels, noiseSTD=kernelsParams0NoiseSTD)
-    kernels = kernels[0] # the current code uses the same kernel for all trials
-    kernelsParams0 = kernelsParams0[0] # the current code uses the same kernel for all trials
+    kernels = utils.svGPFA.configUtils.getKernels(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
+    kernelsParams0 = demoUtils.getKernelsParams0(kernels=kernels, noiseSTD=kernelsParams0NoiseSTD)
+    kernels = kernels[0] # the current code uses the same kernels for all trials
+    kernelsParams0 = kernelsParams0[0] # the current code uses the same kernels for all trials
 
-    qMu0, qSVec0, qSDiag0 = getSVPosteriorOnIndPointsParams0(nIndPointsPerLatent=nIndPointsPerLatent, nLatents=nLatents, nTrials=nTrials, scale=initCondIndPointsScale)
+    qMu0, qSVec0, qSDiag0 = demoUtils.getSVPosteriorOnIndPointsParams0(nIndPointsPerLatent=nIndPointsPerLatent, nLatents=nLatents, nTrials=nTrials, scale=initCondIndPointsScale)
 
-    Z0 = getIndPointLocs0(nIndPointsPerLatent=nIndPointsPerLatent,
+    Z0 = demoUtils.getIndPointLocs0(nIndPointsPerLatent=nIndPointsPerLatent,
                           trialsLengths=trialsLengths, firstIndPoint=firstIndPoint)
 
     qUParams0 = {"qMu0": qMu0, "qSVec0": qSVec0, "qSDiag0": qSDiag0}
@@ -228,14 +192,12 @@ def main(argv):
     # plot lower bound history
     plot.svGPFA.plotUtils.plotLowerBoundHist(lowerBoundHist=lowerBoundHist, figFilename=lowerBoundHistFigFilename)
 
-    # predict latents at test times
-    testMuK, testVarK = model.predictLatents(newTimes=testTimes)
-
     # plot true and estimated latents
+    testMuK, testVarK = model.predictLatents(newTimes=testTimes)
     indPointsLocs = model.getIndPointsLocs()
     plot.svGPFA.plotUtils.plotTrueAndEstimatedLatents(timesEstimatedValues=testTimes, muK=testMuK, varK=testVarK, indPointsLocs=indPointsLocs, timesTrueValues=timesTrueValues, trueLatents=trueLatents, trueLatentsMeans=trueLatentsMeans, trueLatentsSTDs=trueLatentsSTDs, trialToPlot=trialToPlot, figFilename=latentsFigFilename)
 
-    # KS test time rescaling
+    # KS test time rescaling with numerical correction
     T = torch.tensor(trialsLengths).max()
     oneTrialCIFTimes = torch.arange(0, T, dtCIF)
     cifTimes = torch.unsqueeze(torch.ger(torch.ones(nTrials), oneTrialCIFTimes), dim=2)
@@ -247,54 +209,55 @@ def main(argv):
 
     title = "Trial {:d}, Neuron {:d} ({:d} spikes)".format(trialToPlot, neuronToPlot, len(spikesTimesKS))
 
-    diffECDFsX, diffECDFsY, estECDFx, estECDFy, simECDFx, simECDFy, cb = KSTestTimeRescalingNumericalCorrection(spikesTimes=spikesTimesKS, cifTimes=cifTimesKS, cifValues=cifValuesKS, gamma=gamma)
+    diffECDFsX, diffECDFsY, estECDFx, estECDFy, simECDFx, simECDFy, cb = stats.pointProcess.tests.KSTestTimeRescalingNumericalCorrection(spikesTimes=spikesTimesKS, cifTimes=cifTimesKS, cifValues=cifValuesKS, gamma=gamma)
     plot.svGPFA.plotUtils.plotResKSTestTimeRescalingNumericalCorrection(diffECDFsX=diffECDFsX, diffECDFsY=diffECDFsY, estECDFx=estECDFx, estECDFy=estECDFy, simECDFx=simECDFx, simECDFy=simECDFy, cb=cb, figFilename=ksTestTimeRescalingNumericalCorrectionFigFilename, title=title)
 
+    # CIF
     plot.svGPFA.plotUtils.plotSimulatedAndEstimatedCIFs(times=cifTimes[trialToPlot, :, 0], simCIFValues=simCIFsValues[trialToPlot][neuronToPlot], estCIFValues=cifsValuesKS, figFilename=trueAndEstimatedCIFsFigFilename, title=title)
 
+    # ROC predictive analysis
     pk = cifValuesKS*dtCIF
     bins = pd.interval_range(start=0, end=T, periods=len(pk))
-    # start binning spikes using pandas
     cutRes, _ = pd.cut(spikesTimesKS, bins=bins, retbins=True)
     Y = torch.from_numpy(cutRes.value_counts().values)
-
     fpr, tpr, thresholds = metrics.roc_curve(Y, pk, pos_label=1)
     roc_auc = metrics.auc(fpr, tpr)
     plot.svGPFA.plotUtils.plotResROCAnalysis(fpr=fpr, tpr=tpr, auc=roc_auc, title=title, figFilename=rocFigFilename)
 
     # plot model params
-    tLatentsMeansFuncs = getLatentsMeansFuncs(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
-    trueC, trueD = getLinearEmbeddingParams(nNeurons=nNeurons, nLatents=nLatents, config=simInitConfig)
-    trialsTimes = getTrialsTimes(trialsLengths=trialsLengths, dt=dtSimulate)
-
-    # latentsMeansSamples[r][k,t]
-    tLatentsMeans = getLatentsMeanFuncsSamples(latentsMeansFuncs=
+    tLatentsMeansFuncs = utils.svGPFA.configUtils.getLatentsMeansFuncs(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
+    trialsTimes = utils.svGPFA.miscUtils.getTrialsTimes(trialsLengths=trialsLengths, dt=dtSimulate)
+    tLatentsMeans = utils.svGPFA.miscUtils.getLatentsMeanFuncsSamples(latentsMeansFuncs=
                                                 tLatentsMeansFuncs,
                                                trialsTimes=trialsTimes,
-                                               dtype=trueC.dtype)
-
+                                               dtype=C.dtype)
     kernelsParams = model.getKernelsParams()
     with torch.no_grad():
         latentsMeans, _ = model.predictLatents(newTimes=trialsTimes[0])
         estimatedC, estimatedD = model.getSVEmbeddingParams()
+
     plotTrueAndEstimatedKernelsParams(trueKernels=kernels, estimatedKernelsParams=kernelsParams)
     plt.savefig(kernelsParamsFigFilename)
 
     plotTrueAndEstimatedLatentsMeans(trueLatentsMeans=tLatentsMeans, estimatedLatentsMeans=latentsMeans, trialsTimes=trialsTimes)
     plt.savefig(latentsMeansFigFilename)
 
-    plotTrueAndEstimatedEmbeddingParams(trueC=trueC, trueD=trueD, estimatedC=estimatedC, estimatedD=estimatedD)
+    plotTrueAndEstimatedEmbeddingParams(trueC=C, trueD=d, estimatedC=estimatedC, estimatedD=estimatedD)
     plt.savefig(embeddingParamsFigFilename)
 
-    #KS Test analytical correction
+    # KS test time rescaling with analytical correction
     t0 = math.floor(cifTimesKS.min())
     tf = math.ceil(cifTimesKS.max())
     dt = (cifTimesKS[1]-cifTimesKS[0]).item()
     utSRISIs, uCDF, cb, utRISIs = KSTestTimeRescalingAnalyticalCorrectionUnbinned(spikesTimes=spikesTimesKS, cifValues=cifValuesKS, t0=t0, tf=tf, dt=dt)
     sUTRISIs, _ = torch.sort(utSRISIs)
+
     plot.svGPFA.plotUtils.plotResKSTestTimeRescalingAnalyticalCorrection(sUTRISIs=sUTRISIs, uCDF=uCDF, cb=cb, title=title, figFilename=ksTestTimeRescalingAnalyticalCorrectionFigFilename)
+
     plot.svGPFA.plotUtils.plotDifferenceCDFs(sUTRISIs=sUTRISIs, uCDF=uCDF, cb=cb, figFilename=timeRescalingDiffCDFsFigFilename),
-    plot.svGPFA.plotUtils.plotScatter1Lag(x=utRISIs, title=title, figFilename=timeRescaling1LagScatterPlotFigFilename),
+
+    plot.svGPFA.plotUtils.plotScatter1Lag(x=utRISIs, title=title, figFilename=timeRescaling1LagScatterPlotFigFilename)
+
     acfRes, confint = statsmodels.tsa.stattools.acf(x=utRISIs, unbiased=True, alpha=0.05)
     plot.svGPFA.plotUtils.plotACF(acf=acfRes, Fs=1/dt, confint=confint, title=title, figFilename=timeRescalingACFFigFilename),
 
