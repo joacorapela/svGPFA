4dee8b226743b6dd7bada0310432bb45f14b7322
Joaquin Rapela <joacorapela@gmail.com>
2020-06-24 19:12:37 +0100

    Fixed the test. Now I am reversing the points and weights in legGaussVarLimits to make them compatible with Leas' code

--- ci/test_leggaussVarLimits.py
+++ ci/test_leggaussVarLimits.py
@@ -15,29 +15,8 @@ def test_leggaussVarLimits():
     a = mat["a"][0][0]
     b = mat["b"][0][0]
 
-    # Matlab's points are in decreasing order while Pythons ones are in increasing order
-    # This is what I should be able to do
-    # x = torch.from_numpy(mat["x"]).type(torch.DoubleTensor)
-    # x = x[::-1]
-    # But torch does not support negative steps in slices
-    # begin workaround
-    x_tmp = torch.from_numpy(mat["x"][0,:]).type(torch.DoubleTensor)
-    x = torch.empty(x_tmp.shape)
-    for i in range(len(x)):
-        x[i] = x_tmp[len(x_tmp)-1-i]
-    # end workaround
-
-    # Matlab's points are in decreasing order while Pythons ones are in increasing order
-    # This is what I should be able to do
-    # w = torch.from_numpy(mat["w"]).type(torch.DoubleTensor)
-    # w = w[::-1]
-    # But torch does not support negative steps in slices
-    # begin workaround
-    w_tmp = torch.from_numpy(mat["w"][0,:]).type(torch.DoubleTensor)
-    w = torch.empty(w_tmp.shape)
-    for i in range(len(w)):
-        w[i] = w_tmp[len(w_tmp)-1-i]
-    # end workaround
+    x = torch.from_numpy(mat["x"]).type(torch.DoubleTensor)
+    w = torch.from_numpy(mat["w"]).type(torch.DoubleTensor)
 
     px, pw = leggaussVarLimits(n=n, a=a, b=b, dtype=torch.double)
     xDiff = torch.mean((x-px)**2)
