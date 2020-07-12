62aa94b41479f0497232e05cd410412524da8af5
Joaquin Rapela <joacorapela@gmail.com>
2020-07-12 20:37:04 +0100

    Added many more plots

--- scripts/doPlotPointProcess.py
+++ scripts/doPlotPointProcess.py
@@ -11,6 +11,88 @@ import plot.svGPFA.plotUtils
 import matplotlib.pyplot as plt
 
 def main(argv):
+    if len(argv)!=2:
+        print("Usage {:s} <estimation number> ".format(argv[0]))
+        return
+
+    estNumber = int(argv[1])
+
+    modelSaveFilename = "results/{:08d}_estimatedModel.pickle".format(estResNumber)
+
+    # plot lower bound history
+    plot.svGPFA.plotUtils.plotLowerBoundHist(lowerBoundHist=lowerBoundHist, figFilename=lowerBoundHistFigFilename)
+
+    # plot true and estimated latents
+    testMuK, testVarK = model.predictLatents(newTimes=testTimes)
+    indPointsLocs = model.getIndPointsLocs()
+    plot.svGPFA.plotUtils.plotTrueAndEstimatedLatents(timesEstimatedValues=testTimes, muK=testMuK, varK=testVarK, indPointsLocs=indPointsLocs, timesTrueValues=timesTrueValues, trueLatents=trueLatents, trueLatentsMeans=trueLatentsMeans, trueLatentsSTDs=trueLatentsSTDs, trialToPlot=trialToPlot, figFilename=latentsFigFilename)
+
+    # KS test time rescaling with numerical correction
+    T = torch.tensor(trialsLengths).max()
+    oneTrialCIFTimes = torch.arange(0, T, dtCIF)
+    cifTimes = torch.unsqueeze(torch.ger(torch.ones(nTrials), oneTrialCIFTimes), dim=2)
+    with torch.no_grad():
+        cifValues = model.computeMeanCIFs(times=cifTimes)
+    spikesTimesKS = spikesTimes[trialToPlot][neuronToPlot]
+    cifTimesKS = cifTimes[trialToPlot,:,0]
+    cifValuesKS = cifValues[trialToPlot][neuronToPlot]
+
+    title = "Trial {:d}, Neuron {:d} ({:d} spikes)".format(trialToPlot, neuronToPlot, len(spikesTimesKS))
+
+    diffECDFsX, diffECDFsY, estECDFx, estECDFy, simECDFx, simECDFy, cb = stats.pointProcess.tests.KSTestTimeRescalingNumericalCorrection(spikesTimes=spikesTimesKS, cifTimes=cifTimesKS, cifValues=cifValuesKS, gamma=gamma)
+    plot.svGPFA.plotUtils.plotResKSTestTimeRescalingNumericalCorrection(diffECDFsX=diffECDFsX, diffECDFsY=diffECDFsY, estECDFx=estECDFx, estECDFy=estECDFy, simECDFx=simECDFx, simECDFy=simECDFy, cb=cb, figFilename=ksTestTimeRescalingNumericalCorrectionFigFilename, title=title)
+
+    # CIF
+#     plot.svGPFA.plotUtils.plotSimulatedAndEstimatedCIFs(times=cifTimes[trialToPlot, :, 0], simCIFValues=simCIFsValues[trialToPlot][neuronToPlot], estCIFValues=cifsValuesKS, figFilename=trueAndEstimatedCIFsFigFilename, title=title)
+
+    # ROC predictive analysis
+    pk = cifValuesKS*dtCIF
+    bins = pd.interval_range(start=0, end=T, periods=len(pk))
+    cutRes, _ = pd.cut(spikesTimesKS, bins=bins, retbins=True)
+    Y = torch.from_numpy(cutRes.value_counts().values)
+    fpr, tpr, thresholds = metrics.roc_curve(Y, pk, pos_label=1)
+    roc_auc = metrics.auc(fpr, tpr)
+    plot.svGPFA.plotUtils.plotResROCAnalysis(fpr=fpr, tpr=tpr, auc=roc_auc, title=title, figFilename=rocFigFilename)
+
+    # plot model params
+    tLatentsMeansFuncs = utils.svGPFA.configUtils.getLatentsMeansFuncs(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
+    trialsTimes = utils.svGPFA.miscUtils.getTrialsTimes(trialsLengths=trialsLengths, dt=dtSimulate)
+    tLatentsMeans = utils.svGPFA.miscUtils.getLatentsMeanFuncsSamples(latentsMeansFuncs=
+                                                tLatentsMeansFuncs,
+                                               trialsTimes=trialsTimes,
+                                               dtype=C.dtype)
+    kernelsParams = model.getKernelsParams()
+    with torch.no_grad():
+        latentsMeans, _ = model.predictLatents(newTimes=trialsTimes[0])
+        estimatedC, estimatedD = model.getSVEmbeddingParams()
+
+    plotTrueAndEstimatedKernelsParams(trueKernels=kernels, estimatedKernelsParams=kernelsParams)
+    plt.savefig(kernelsParamsFigFilename)
+
+    plotTrueAndEstimatedLatentsMeans(trueLatentsMeans=tLatentsMeans, estimatedLatentsMeans=latentsMeans, trialsTimes=trialsTimes)
+    plt.savefig(latentsMeansFigFilename)
+
+    plotTrueAndEstimatedEmbeddingParams(trueC=C, trueD=d, estimatedC=estimatedC, estimatedD=estimatedD)
+    plt.savefig(embeddingParamsFigFilename)
+
+    # KS test time rescaling with analytical correction
+    t0 = math.floor(cifTimesKS.min())
+    tf = math.ceil(cifTimesKS.max())
+    dt = (cifTimesKS[1]-cifTimesKS[0]).item()
+    utSRISIs, uCDF, cb, utRISIs = KSTestTimeRescalingAnalyticalCorrectionUnbinned(spikesTimes=spikesTimesKS, cifValues=cifValuesKS, t0=t0, tf=tf, dt=dt)
+    sUTRISIs, _ = torch.sort(utSRISIs)
+
+    plot.svGPFA.plotUtils.plotResKSTestTimeRescalingAnalyticalCorrection(sUTRISIs=sUTRISIs, uCDF=uCDF, cb=cb, title=title, figFilename=ksTestTimeRescalingAnalyticalCorrectionFigFilename)
+
+    plot.svGPFA.plotUtils.plotDifferenceCDFs(sUTRISIs=sUTRISIs, uCDF=uCDF, cb=cb, figFilename=timeRescalingDiffCDFsFigFilename),
+
+    plot.svGPFA.plotUtils.plotScatter1Lag(x=utRISIs, title=title, figFilename=timeRescaling1LagScatterPlotFigFilename)
+
+    acfRes, confint = statsmodels.tsa.stattools.acf(x=utRISIs, unbiased=True, alpha=0.05)
+    plot.svGPFA.plotUtils.plotACF(acf=acfRes, Fs=1/dt, confint=confint, title=title, figFilename=timeRescalingACFFigFilename),
+
+
+
     if len(argv)!=3:
         print("Usage {:s} <random prefix> <trial to plot>".format(argv[0]))
         return
