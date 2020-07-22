8412312ccac4c0865ce59a1f9da1883654286799
Joaquin Rapela <joacorapela@gmail.com>
2020-07-22 13:50:39 +0100

    Updates to use chol representation of inducing points covariance

--- src/stats/svGPFA/svPosteriorOnIndPoints.py
+++ src/stats/svGPFA/svPosteriorOnIndPoints.py
@@ -1,7 +1,6 @@
 
 import pdb
 import torch
-from .utils import build3DdiagFromDiagVector
 
 class SVPosteriorOnIndPoints:
 
@@ -11,30 +10,25 @@ class SVPosteriorOnIndPoints:
     def setInitialParams(self, initialParams):
         nLatents = len(initialParams["qMu0"])
         self._qMu = [initialParams["qMu0"][k] for k in range(nLatents)]
-        self._qSVec = [initialParams["qSVec0"][k] for k in range(nLatents)]
-        self._qSDiag = [initialParams["qSDiag0"][k] for k in range(nLatents)]
+        self._qSRSigma = [initialParams["qSRSigma0"][k] for k in range(nLatents)]
 
     def getParams(self):
         listOfTensors = []
         listOfTensors.extend([self._qMu[k] for k in range(len(self._qMu))])
-        listOfTensors.extend([self._qSVec[k] for k in range(len(self._qSVec))])
-        listOfTensors.extend([self._qSDiag[k] for k in range(len(self._qSDiag))])
+        listOfTensors.extend([self._qSRSigma[k] for k in range(len(self._qSRSigma))])
         return listOfTensors
 
     def getQMu(self):
         return self._qMu
 
     def buildQSigma(self):
-        R = self._qSVec[0].shape[0]
-        K = len(self._qSVec)
+        K = len(self._qSRSigma)
+        R = self._qSRSigma[0].shape[0]
         qSigma = [[None] for k in range(K)]
         for k in range(K):
-            nIndK = self._qSDiag[k].shape[1]
-            # qq \in nTrials x nInd[k] x 1
-            qq = self._qSVec[k].reshape(shape=(R, nIndK, 1))
-            # dd \in nTrials x nInd[k] x 1
-            nIndKVarRnkK = self._qSVec[k].shape[1]
-            dd = build3DdiagFromDiagVector(v=(self._qSDiag[k].flatten())**2, M=R, N=nIndKVarRnkK)
-            qSigma[k] = torch.matmul(qq, torch.transpose(a=qq, dim0=1, dim1=2)) + dd
+            nIndPointsK = self._qSRSigma[k].shape[1]
+            qSigma[k] = torch.empty((R, nIndPointsK, nIndPointsK), dtype=torch.double)
+            for r in range(R):
+                qSigma[k][r,:,:] = torch.matmul(self._qSRSigma[k][r,:,:], torch.transpose(self._qSRSigma[k][r,:,:], 0, 1))
         return(qSigma)
 
