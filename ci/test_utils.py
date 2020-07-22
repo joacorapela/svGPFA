d57e6aefdfc19482b374de41f08ece449b88a16c
Joaquin Rapela <joacorapela@gmail.com>
2020-07-22 13:47:37 +0100

    Moved stats.svGPFA.utils to utils.svGPFA.initUtils

--- ci/test_utils.py
+++ ci/test_utils.py
@@ -6,21 +6,21 @@ import math
 from scipy.io import loadmat
 import torch
 sys.path.append("../src")
-import stats.svGPFA.utils
+import utils.svGPFA.initUtils
 
 def test_getDiagIndicesIn3DArray():
     N = 3
     M = 2
     trueDiagIndices = torch.tensor([0, 4, 8, 9, 13, 17])
 
-    diagIndices = stats.svGPFA.utils.getDiagIndicesIn3DArray(N=N, M=M)
+    diagIndices = utils.svGPFA.initUtils.getDiagIndicesIn3DArray(N=N, M=M)
     assert(((trueDiagIndices-diagIndices)**2).sum()==0)
 
 def test_build3DdiagFromDiagVector():
     N = 3
     M = 2
     v = torch.arange(M*N, dtype=torch.double)
-    D = stats.svGPFA.utils.build3DdiagFromDiagVector(v=v, N=N, M=M)
+    D = utils.svGPFA.initUtils.build3DdiagFromDiagVector(v=v, N=N, M=M)
     trueD = torch.tensor([[[0,0,0],[0,1,0],[0,0,2]],[[3,0,0],[0,4,0],[0,0,5]]], dtype=torch.double)
     assert(((trueD-D)**2).sum()==0)
 
