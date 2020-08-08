530edd9623dd76bf8e1ea6f66066f1786c22ddbb
Joaquin Rapela <joacorapela@gmail.com>
2020-08-08 14:44:10 +0100

    Kernels are not trial specific now

--- src/utils/svGPFA/initUtils.py
+++ src/utils/svGPFA/initUtils.py
@@ -37,7 +37,7 @@ def buildQSigmaFromQSVecAndQSDiag(qSVec, qSDiag):
     return(qSigma)
 
 
-def getIndPointLocs0(nIndPointsPerLatent, trialsLengths, firstIndPoint):
+def getIndPointLocs0(nIndPointsPerLatent, trialsLengths, firstIndPointLoc):
     nLatents = len(nIndPointsPerLatent)
     nTrials = len(trialsLengths)
 
@@ -46,7 +46,7 @@ def getIndPointLocs0(nIndPointsPerLatent, trialsLengths, firstIndPoint):
         Z0[i] = torch.empty((nTrials, nIndPointsPerLatent[i], 1), dtype=torch.double)
     for i in range(nLatents):
         for j in range(nTrials):
-            Z0[i][j,:,0] = torch.linspace(firstIndPoint, trialsLengths[j], nIndPointsPerLatent[i])
+            Z0[i][j,:,0] = torch.linspace(firstIndPointLoc, trialsLengths[j], nIndPointsPerLatent[i])
     return Z0
 
 def getSVPosteriorOnIndPointsParams0(nIndPointsPerLatent, nLatents, nTrials, scale):
@@ -54,19 +54,17 @@ def getSVPosteriorOnIndPointsParams0(nIndPointsPerLatent, nLatents, nTrials, sca
     qSVec0 = [None]*nLatents
     qSDiag0 = [None]*nLatents
     for i in range(nLatents):
+        # qMu0[i] = torch.rand(nTrials, nIndPointsPerLatent[i], 1, dtype=torch.double)
         qMu0[i] = torch.zeros(nTrials, nIndPointsPerLatent[i], 1, dtype=torch.double)
         qSVec0[i] = scale*torch.eye(nIndPointsPerLatent[i], 1, dtype=torch.double).repeat(nTrials, 1, 1)
         qSDiag0[i] = scale*torch.ones(nIndPointsPerLatent[i], 1, dtype=torch.double).repeat(nTrials, 1, 1)
     return qMu0, qSVec0, qSDiag0
 
 def getKernelsParams0(kernels, noiseSTD):
-    nTrials = len(kernels)
-    nLatents = len(kernels[0])
-    kernelsParams0 = [[] for r in range(nTrials)]
-    for r in range(nTrials):
-        kernelsParams0[r] = [[] for r in range(nLatents)]
-        for k in range(nLatents):
-            trueParams = kernels[r][k].getParams()
-            kernelsParams0[r][k] = noiseSTD*torch.randn(len(trueParams))+trueParams
+    nLatents = len(kernels)
+    kernelsParams0 = [[] for r in range(nLatents)]
+    for k in range(nLatents):
+        trueParams = kernels[k].getParams()
+        kernelsParams0[k] = noiseSTD*torch.randn(len(trueParams))+trueParams
     return kernelsParams0
 
