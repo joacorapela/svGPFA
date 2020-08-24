8d965878c521631fb32e0b20125b59fd65d5865b
Joaquin Rapela <joacorapela@gmail.com>
2020-08-24 11:53:56 +0100

    Added argparser and plotting one trial and latent

--- scripts/doPlotTrueAndEstimatedParamsChol.py
+++ scripts/doPlotTrueAndEstimatedParamsChol.py
@@ -3,35 +3,46 @@ import sys
 import os
 import pdb
 import pickle
+import argparse
 import configparser
 import numpy as np
 import torch
 import plotly.graph_objects as go
 import plotly
-from plotly.subplots import make_subplots
 import plotly.tools as tls
+import plotly.io as pio
 sys.path.append("../src")
 import utils.svGPFA.configUtils
 import utils.svGPFA.miscUtils
 import plot.svGPFA.plotUtilsPlotly
 
 def main(argv):
-    if len(argv)!=2:
-        print("Usage {:s} <estimation number> ".format(argv[0]))
-        return
+    parser = argparse.ArgumentParser()
+    parser.add_argument("estResNumber", help="Estimation result number", type=int)
+    parser.add_argument("--latentToPlot", help="Latent to plot", type=int, default=0)
+    parser.add_argument("--trialToPlot", help="Trial to plot", type=int, default=0)
+    parser.add_argument("--neuronToPlot", help="Neuron to plot", type=int, default=0)
+    args = parser.parse_args()
 
-    estNumber = int(argv[1])
-    gpRegularization = 1e-3
-    estMetaDataFilename = "results/{:08d}_estimation_metaData.ini".format(estNumber)
-    modelSaveFilename = "results/{:08d}_estimatedModel.pickle".format(estNumber)
-    kernelsParamsFigFilenamePattern = "figures/{:08d}_trueAndEstimatedKernelsParams.{{:s}}".format(estNumber)
-    latentsMeansFigFilenamePattern = "figures/{:08d}_trueAndEstimatedLatentsMeans.{{:s}}".format(estNumber)
-    embeddingParamsFigFilenamePattern = "figures/{:08d}_trueAndEstimatedEmbeddingParams.{{:s}}".format(estNumber)
+    estResNumber = args.estResNumber
+    latentToPlot = args.latentToPlot
+    trialToPlot = args.trialToPlot
+    neuronToPlot = args.neuronToPlot
+
+    estMetaDataFilename = "results/{:08d}_estimation_metaData.ini".format(estResNumber)
+    modelSaveFilename = "results/{:08d}_estimatedModel.pickle".format(estResNumber)
+    latentsFigFilenamePattern = "figures/{:08d}_trueAndEstimatedLatents_latent{:03d}_trial_{:03d}.{{:s}}".format(estResNumber, latentToPlot, trialToPlot)
+    indPointsMeanFigFilenamePattern = "figures/{:08d}_trueAndEstimatedIndPointsMeans_latent{:03d}_trial_{:03d}.{{:s}}".format(estResNumber, latentToPlot, trialToPlot)
+    indPointsCovFigFilenamePattern = "figures/{:08d}_trueAndEstimatedIndPointsCovs_latent{:03d}_trial_{:03d}.{{:s}}".format(estResNumber, latentToPlot, trialToPlot)
+    indPointsLocsFigFilenamePattern = "figures/{:08d}_trueAndEstimatedIndPointsLocs_latent{:03d}_trial_{:03d}.{{:s}}".format(estResNumber, latentToPlot, trialToPlot)
+    kernelsParamsFigFilenamePattern = "figures/{:08d}_trueAndEstimatedKernelsParams.{{:s}}".format(estResNumber)
+    embeddingParamsFigFilenamePattern = "figures/{:08d}_trueAndEstimatedEmbeddingParams.{{:s}}".format(estResNumber)
 
     estMetaDataConfig = configparser.ConfigParser()
     estMetaDataConfig.read(estMetaDataFilename)
     simResNumber = int(estMetaDataConfig["simulation_params"]["simResNumber"])
     simMetaDataFilename = "results/{:08d}_simulation_metaData.ini".format(simResNumber)
+    simResFilename = "results/{:08d}_simRes.pickle".format(simResNumber)
     simMetaDataConfig = configparser.ConfigParser()
     simMetaDataConfig.read(simMetaDataFilename)
     simInitConfigFilename = simMetaDataConfig["simulation_params"]["simInitConfigFilename"]
@@ -43,45 +54,101 @@ def main(argv):
     trialsLengths = [float(str) for str in simInitConfig["control_variables"]["trialsLengths"][1:-1].split(",")]
     nTrials = len(trialsLengths)
     dtSimulate = float(simInitConfig["control_variables"]["dtCIF"])
+    tIndPointsMeans = utils.svGPFA.configUtils.getIndPointsMeans(nTrials=nTrials, nLatents=nLatents, config=simInitConfig)
+
+    with open(simResFilename, "rb") as f: simRes = pickle.load(f)
+    tKzzChol = simRes["KzzChol"]
+    tIndPointsCovs = [torch.matmul(tKzzChol[k], torch.transpose(tKzzChol[k], 1, 2)) for k in range(nLatents)]
+    tIndPointsLocs = simRes["indPointsLocs"]
+    tTimes = simRes["times"]
+    tLatentsSamples = simRes["latents"]
+    tLatentsMeans = simRes["latentsMeans"]
+    tLatentsSTDs = simRes["latentsSTDs"]
 
     kernels = utils.svGPFA.configUtils.getKernels(nLatents=nLatents, config=simInitConfig, forceUnitScale=True)
     # latentsMeansFuncs[r][k] \in lambda(t)
 #     tLatentsMeansFuncs = utils.svGPFA.configUtils.getLatentsMeansFuncs(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
     CFilename = simInitConfig["embedding_params"]["C_filename"]
     dFilename = simInitConfig["embedding_params"]["d_filename"]
-    trueC, trueD = utils.svGPFA.configUtils.getLinearEmbeddingParams(nNeurons=nNeurons, nLatents=nLatents, CFilename=CFilename, dFilename=dFilename)
+    trueC, trueD = utils.svGPFA.configUtils.getLinearEmbeddingParams(CFilename=CFilename, dFilename=dFilename)
     trialsTimes = utils.svGPFA.miscUtils.getTrialsTimes(trialsLengths=trialsLengths, dt=dtSimulate)
-
-    # latentsMeansSamples[r][k,t]
-#     tLatentsMeans = utils.svGPFA.miscUtils.getLatentsMeanFuncsSamples(latentsMeansFuncs=
-#                                                 tLatentsMeansFuncs,
-#                                                trialsTimes=trialsTimes,
-#                                                dtype=trueC.dtype)
 
     with open(modelSaveFilename, "rb") as f: savedResults = pickle.load(f)
     model = savedResults["model"]
     kernelsParams = model.getKernelsParams()
+    eTimes = tTimes
     with torch.no_grad():
-        latentsMeans, _ = model.predictLatents(newTimes=trialsTimes[0])
+        eLatentsMeans, eLatentsSTDs = model.predictLatents(newTimes=eTimes[0])
     estimatedC, estimatedD = model.getSVEmbeddingParams()
-    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedKernelsParams(
-        trueKernels=kernels, 
-        estimatedKernelsParams=kernelsParams)
+    eIndPointsLocs = model.getIndPointsLocs()
+
+    pio.renderers.default = "browser"
+
+    tLatentsSamplesToPlot = tLatentsSamples[trialToPlot][latentToPlot,:]
+    tLatentsMeansToPlot = tLatentsMeans[trialToPlot][latentToPlot,:]
+    tLatentsSTDsToPlot = tLatentsSTDs[trialToPlot][latentToPlot,:]
+    eLatentsMeansToPlot = eLatentsMeans[trialToPlot,:,latentToPlot]
+    eLatentsSTDsToPlot = eLatentsSTDs[trialToPlot,:,latentToPlot]
+    title = "Trial {:d}, Latent {:d}".format(trialToPlot, latentToPlot)
+    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedLatentsOneTrialOneLatent(
+        tTimes = tTimes[0],
+        tLatentsSamples=tLatentsSamplesToPlot,
+        tLatentsMeans=tLatentsMeansToPlot,
+        tLatentsSTDs=tLatentsSTDsToPlot,
+        eTimes=eTimes[0],
+        eLatentsMeans=eLatentsMeansToPlot,
+        eLatentsSTDs=eLatentsSTDsToPlot,
+        title=title,
+    )
+    fig.write_image(latentsFigFilenamePattern.format("png"))
+    fig.write_html(latentsFigFilenamePattern.format("html"))
+    fig.show()
+
+    tKernelToPlot = kernels[latentToPlot]
+    eKernelParamsToPlot = kernelsParams[latentToPlot]
+    title = "Latent {:d}".format(latentToPlot)
+    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedKernelsParamsOneLatent(trueKernel=tKernelToPlot, estimatedKernelParams=eKernelParamsToPlot, title=title)
     fig.write_image(kernelsParamsFigFilenamePattern.format("png"))
     fig.write_html(kernelsParamsFigFilenamePattern.format("html"))
+    fig.show()
 
-    # qMu[r] \in nTrials x nInd[k] x 1
-#     plot.svGPFA.plotUtilsPlotly.plotTrueAndEstimatedLatentsMeans(trueLatentsMeans=tLatentsMeans,
-#                                      estimatedLatentsMeans=latentsMeans,
-#                                      trialsTimes=trialsTimes)
-#     fig.write_image(latentsMeansFigFilenamePattern.format("png"))
-#     fig.write_html(latentsMeansFigFilenamePattern.format("html"))
+    svPosteriorOnIndPointsParams = model.getSVPosteriorOnIndPointsParams()
 
-    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedEmbeddingParams(
-        trueC=trueC, trueD=trueD,
-        estimatedC=estimatedC, estimatedD=estimatedD)
+    eIndPointsMeans = svPosteriorOnIndPointsParams[:nLatents]
+    tIndPointsMeansToPlot = tIndPointsMeans[trialToPlot][latentToPlot][:,0]
+    eIndPointsMeansToPlot = eIndPointsMeans[latentToPlot][trialToPlot,:,0]
+    title = "Trial {:d}, Latent {:d}".format(trialToPlot, latentToPlot)
+    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedIndPointsMeansOneTrialOneLatent(trueIndPointsMeans=tIndPointsMeansToPlot, estimatedIndPointsMeans=eIndPointsMeansToPlot, title=title,)
+    fig.write_image(indPointsMeanFigFilenamePattern.format("png"))
+    fig.write_html(indPointsMeanFigFilenamePattern.format("html"))
+    fig.show()
+
+    srQSigmaVecs = svPosteriorOnIndPointsParams[nLatents:]
+    eIndPointsCovs = utils.svGPFA.miscUtils.buildQSigmasFromSRQSigmaVecs(srQSigmaVecs=srQSigmaVecs)
+    tIndPointsCovToPlot = tIndPointsCovs[latentToPlot][trialToPlot,:,:]
+    eIndPointsCovToPlot = eIndPointsCovs[latentToPlot][trialToPlot,:,:]
+    title = "Trial {:d}, Latent {:d}".format(trialToPlot, latentToPlot)
+    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedIndPointsCovsOneTrialOneLatent(
+        trueIndPointsCov=tIndPointsCovToPlot,
+        estimatedIndPointsCov=eIndPointsCovToPlot,
+        title=title,
+    )
+    fig.write_image(indPointsCovFigFilenamePattern.format("png"))
+    fig.write_html(indPointsCovFigFilenamePattern.format("html"))
+    fig.show()
+
+    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedEmbeddingParams(trueC=trueC, trueD=trueD, estimatedC=estimatedC, estimatedD=estimatedD)
     fig.write_image(embeddingParamsFigFilenamePattern.format("png"))
     fig.write_html(embeddingParamsFigFilenamePattern.format("html"))
+    fig.show()
+
+    tIndPointsLocsToPlot = tIndPointsLocs[latentToPlot][trialToPlot,:,0]
+    eIndPointsLocsToPlot = eIndPointsLocs[latentToPlot][trialToPlot,:,0]
+    title = "Trial {:d}, Latent {:d}".format(trialToPlot, latentToPlot)
+    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedIndPointsLocsOneTrialOneLatent(trueIndPointsLocs=tIndPointsLocsToPlot, estimatedIndPointsLocs=eIndPointsLocsToPlot, title=title)
+    fig.write_image(indPointsLocsFigFilenamePattern.format("png"))
+    fig.write_html(indPointsLocsFigFilenamePattern.format("html"))
+    fig.show()
 
     pdb.set_trace()
 
