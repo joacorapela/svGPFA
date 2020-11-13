Fixed problem in the calculation of condition number

scripts/doPlotConditionNumberOn1DParamGridTrueParams.py
@@ -125,8 +125,10 @@ def main(argv):
     for i in range(len(paramValues)):
         paramUpdateFun(model=model, paramValue=paramValues[i], trial=trial, latent=latent, neuron=neuron, kernelParamIndex=kernelParamIndex, indPointIndex=indPointIndex, indPointIndex2=indPointIndex2)
         Kzz = model._eLL._svEmbeddingAllTimes._svPosteriorOnLatents._indPointsLocsKMS._Kzz
+#         if paramValues[i]>=0.66:
+#             pdb.set_trace()
         eValues, _ = torch.eig(Kzz[0][trial,:,:])
-        conditionNumbers[i] = eValues[0,0]/eValues[-1,0]
+        conditionNumbers[i] = eValues[:,0].max()/eValues[:,0].min()
 
     title = lowerBoundVsOneParamUtils.getParamTitle(paramType=paramType, trial=trial, latent=latent, neuron=neuron, kernelParamIndex=kernelParamIndex, indPointIndex=indPointIndex, indPointIndex2=indPointIndex2, indPointsLocsKMSRegEpsilon=indPointsLocsKMSRegEpsilon)
     figFilenamePattern = condNumberVsOneParamUtils.getFigFilenamePattern(prefixNumber=simResNumber, descriptor="trueParam", paramType=paramType, trial=trial, latent=latent, neuron=neuron, indPointsLocsKMSRegEpsilon=indPointsLocsKMSRegEpsilon, kernelParamIndex=kernelParamIndex, indPointIndex=indPointIndex, indPointIndex2=indPointIndex2)