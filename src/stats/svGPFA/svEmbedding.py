be6b54137af82adf43033504bc3ee12e7aa65156
Joaquin Rapela <joacorapela@gmail.com>
2021-03-02 16:49:23 +0000

    Setting/getting flattened_params using tensors instead of lists

--- src/stats/svGPFA/svEmbedding.py
+++ src/stats/svGPFA/svEmbedding.py
@@ -71,23 +71,19 @@ class LinearSVEmbedding(SVEmbedding):
         return [self._C, self._d]
 
     def get_flattened_params(self):
-        flattened_params = []
-        flattened_params.extend(self._C.flatten().tolist())
-        flattened_params.extend(self._d.flatten().tolist())
+        flattened_params = torch.cat((self._C.flatten(), self._d.flatten()))
         return flattened_params
 
     def get_flattened_params_grad(self):
-        flattened_params_grad = []
-        flattened_params_grad.extend(self._C.grad.flatten().tolist())
-        flattened_params_grad.extend(self._d.grad.flatten().tolist())
+        flattened_params_grad = torch.cat((self._C.grad.flatten(), self._d.grad.flatten()))
         return flattened_params_grad
 
     def set_params_from_flattened(self, flattened_params):
         flattened_param = flattened_params[:self._C.numel()]
-        self._C = torch.tensor(flattened_param, dtype=torch.double).reshape(self._C.shape)
+        self._C = flattened_param.reshape(self._C.shape)
         flattened_params = flattened_params[self._C.numel():]
         flattened_param = flattened_params[:self._d.numel()]
-        self._d = torch.tensor(flattened_param, dtype=torch.double).reshape(self._d.shape)
+        self._d = flattened_param.reshape(self._d.shape)
         flattened_param = flattened_params[self._d.numel():]
 
     def set_params_requires_grad(self, requires_grad):
