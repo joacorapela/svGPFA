aaa8651f6c094a4c5a20ca2a390f3ddc966e738f
Joaquin Rapela <joacorapela@gmail.com>
2021-03-02 19:17:08 +0000

    Changed model

--- scripts/doDebug.py
+++ scripts/doDebug.py
@@ -4,9 +4,11 @@ import pickle
 sys.path.append("../src")
 
 def main(argv):
-    with open("results/70599799_estimatedModel.pickle", "rb") as f:
+    with open("results/52060814_estimatedModel.pickle", "rb") as f:
         maxRes = pickle.load(f)
     model = maxRes["model"]
+    pdb.set_trace()
+    model.buildKernelsMatrices()
     model.eval()
     print("About to finish ...")
     pdb.set_trace()
