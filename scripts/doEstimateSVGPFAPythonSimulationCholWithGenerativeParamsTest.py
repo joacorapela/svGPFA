74ad947de11a114e9c1b5e6b3b9550f11df6aece
Joaquin Rapela <joacorapela@gmail.com>
2021-03-03 19:47:39 +0000

    Re-arrengements to make it similar to doEstimateSVGPFAPythonSimulationChol.py

--- scripts/doEstimateSVGPFAPythonSimulationCholWithGenerativeParamsTest.py
+++ scripts/doEstimateSVGPFAPythonSimulationCholWithGenerativeParamsTest.py
@@ -28,14 +28,31 @@ def main(argv):
     simResNumber = args.simResNumber
     estInitNumber = args.estInitNumber
 
+    # load data and initial values
+    simResConfigFilename = "results/{:08d}_simulation_metaData.ini".format(simResNumber)
+    simResConfig = configparser.ConfigParser()
+    simResConfig.read(simResConfigFilename)
+    simInitConfigFilename = simResConfig["simulation_params"]["simInitConfigFilename"]
+    simResFilename = simResConfig["simulation_results"]["simResFilename"]
+
+    simInitConfig = configparser.ConfigParser()
+    simInitConfig.read(simInitConfigFilename)
+    nLatents = int(simInitConfig["control_variables"]["nLatents"])
+    nNeurons = int(simInitConfig["control_variables"]["nNeurons"])
+    trialsLengths = [float(str) for str in simInitConfig["control_variables"]["trialsLengths"][1:-1].split(",")]
+    nTrials = len(trialsLengths)
+
+    with open(simResFilename, "rb") as f: simRes = pickle.load(f)
+    spikesTimes = simRes["spikes"]
+    # KzzChol = simRes["KzzChol"]
     estInitConfigFilename = "data/{:08d}_estimation_metaData.ini".format(estInitNumber)
     estInitConfig = configparser.ConfigParser()
     estInitConfig.read(estInitConfigFilename)
-    nQuad = int(estInitConfig["control_variables"]["nQuad"])
-
     randomEmbedding = estInitConfig["control_variables"]["randomEmbedding"].lower()=="true"
     if not randomEmbedding:
         initCondEmbeddingSTD = float(estInitConfig["control_variables"]["initCondEmbeddingSTD"])
+    indPointsLocsKMSRegEpsilon = float(estInitConfig["control_variables"]["indPointsLocsKMSRegEpsilon"])
+    nQuad = int(estInitConfig["control_variables"]["nQuad"])
 
     optimParamsConfig = estInitConfig._sections["optim_params"]
     optimParams = {}
@@ -57,29 +74,6 @@ def main(argv):
            estPrefixUsed = False
     modelSaveFilename = "results/{:08d}_estimatedModel.pickle".format(estResNumber)
 
-    # load data and initial values
-    simResConfigFilename = "results/{:08d}_simulation_metaData.ini".format(simResNumber)
-    simResConfig = configparser.ConfigParser()
-    simResConfig.read(simResConfigFilename)
-    simInitConfigFilename = simResConfig["simulation_params"]["simInitConfigFilename"]
-    simResFilename = simResConfig["simulation_results"]["simResFilename"]
-
-    simInitConfig = configparser.ConfigParser()
-    simInitConfig.read(simInitConfigFilename)
-    nLatents = int(simInitConfig["control_variables"]["nLatents"])
-    nNeurons = int(simInitConfig["control_variables"]["nNeurons"])
-    trialsLengths = [float(str) for str in simInitConfig["control_variables"]["trialsLengths"][1:-1].split(",")]
-    nTrials = len(trialsLengths)
-    # firstIndPointLoc = float(simInitConfig["control_variables"]["firstIndPointLoc"])
-    # indPointsLocsKMSRegEpsilon = float(simInitConfig["control_variables"]["indPointsLocsKMSRegEpsilon"])
-    indPointsLocsKMSRegEpsilon = float(estInitConfig["control_variables"]["indPointsLocsKMSRegEpsilon"])
-
-    with open(simResFilename, "rb") as f: simRes = pickle.load(f)
-    spikesTimes = simRes["spikes"]
-    KzzChol = simRes["KzzChol"]
-#     indPointsMeans = simRes["indPointsMeans"]
-    indPointsMeans = utils.svGPFA.configUtils.getVariationalMean0(nLatents=nLatents, nTrials=nTrials, config=estInitConfig)
-
     if randomEmbedding:
         C0 = torch.rand(nNeurons, nLatents, dtype=torch.double)-0.5*2
         d0 = torch.rand(nNeurons, 1, dtype=torch.double)-0.5*2
@@ -90,19 +84,16 @@ def main(argv):
         C0 = C + torch.randn(C.shape)*initCondEmbeddingSTD
         d0 = d + torch.randn(d.shape)*initCondEmbeddingSTD
 
-    # C, d = utils.svGPFA.configUtils.getLinearEmbeddingParams(CFilename=simInitConfig["embedding_params"]["C_filename"], dFilename=simInitConfig["embedding_params"]["d_filename"])
-
     legQuadPoints, legQuadWeights = utils.svGPFA.miscUtils.getLegQuadPointsAndWeights(nQuad=nQuad, trialsLengths=trialsLengths)
 
-    # kernels = utils.svGPFA.configUtils.getKernels(nLatents=nLatents, config=simInitConfig, forceUnitScale=True)
-    # kernels = utils.svGPFA.configUtils.getKernels(nLatents=nLatents, config=estInitConfig, forceUnitScale=True)
     kernels = utils.svGPFA.configUtils.getScaledKernels(nLatents=nLatents, config=estInitConfig, forceUnitScale=True)["kernels"]
     kernelsParams0 = utils.svGPFA.initUtils.getKernelsParams0(kernels=kernels, noiseSTD=0.0)
 
-    # Z0 = utils.svGPFA.configUtils.getIndPointsLocs0(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
     Z0 = utils.svGPFA.configUtils.getIndPointsLocs0(nLatents=nLatents, nTrials=nTrials, config=estInitConfig)
     nIndPointsPerLatent = [Z0[k].shape[1] for k in range(nLatents)]
 
+#     indPointsMeans = simRes["indPointsMeans"]
+    indPointsMeans = utils.svGPFA.configUtils.getVariationalMean0(nLatents=nLatents, nTrials=nTrials, config=estInitConfig)
     # patch to acommodate Lea's equal number of inducing points across trials
     qMu0 = [[] for k in range(nLatents)]
     for k in range(nLatents):
@@ -111,17 +102,8 @@ def main(argv):
             qMu0[k][r,:,:] = indPointsMeans[r][k]
     # end patch
 
-    # srQSigma0Vecs = utils.svGPFA.initUtils.getSRQSigmaVecsFromSRMatrices(srMatrices=KzzChol)
     qSigma0 = utils.svGPFA.configUtils.getVariationalCov0(nLatents=nLatents, nTrials=nTrials, config=estInitConfig)
     srQSigma0Vecs = utils.svGPFA.initUtils.getSRQSigmaVecsFromSRMatrices(srMatrices=qSigma0)
-    # epsilonSRQSigma0 = 1e4
-    # srQSigma0s = []
-    # for k in range(nLatents):
-    #     srQSigma0sForLatent = torch.empty((nTrials, nIndPointsPerLatent[k], nIndPointsPerLatent[k]))
-    #     for r in range(nTrials):
-    #         srQSigma0sForLatent[r,:,:] = epsilonSRQSigma0*torch.eye(nIndPointsPerLatent[k])
-    #     srQSigma0s.append(srQSigma0sForLatent)
-    # srQSigma0Vecs = utils.svGPFA.initUtils.getSRQSigmaVecsFromSRMatrices(srMatrices=srQSigma0s)
 
     qUParams0 = {"qMu0": qMu0, "srQSigma0Vecs": srQSigma0Vecs}
     kmsParams0 = {"kernelsParams0": kernelsParams0,
