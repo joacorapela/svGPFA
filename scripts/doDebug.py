718d8d28cc12aa42752992a0810f3d89a8bc20ce
Joaquin Rapela <joacorapela@gmail.com>
2021-03-03 16:44:56 +0000

    Dummy change

--- scripts/doDebug.py
+++ scripts/doDebug.py
@@ -4,14 +4,12 @@ import pickle
 sys.path.append("../src")
 
 def main(argv):
-    with open("results/52060814_estimatedModel.pickle", "rb") as f:
+    with open("results/05393918_estimatedModel.pickle", "rb") as f:
         maxRes = pickle.load(f)
     model = maxRes["model"]
     pdb.set_trace()
-    model.buildKernelsMatrices()
     model.eval()
-    print("About to finish ...")
     pdb.set_trace()
-    # break ../src/stats/svGPFA/klDivergence.py:41
+
 if __name__=="__main__":
     main(sys.argv)
