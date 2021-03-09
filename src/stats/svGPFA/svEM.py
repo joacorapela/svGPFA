25cfe248e116bf70be6e9137c063c596f977f40c
Joaquin Rapela <joacorapela@gmail.com>
2021-03-09 21:53:40 +0000

    Printing traceback if error in estimation

--- src/stats/svGPFA/svEM.py
+++ src/stats/svGPFA/svEM.py
@@ -7,6 +7,7 @@ import math
 import pickle
 import numpy as np
 import torch
+import traceback
 
 class TerminationInfo:
     def __init__(self, message):
@@ -94,7 +95,21 @@ class SVEM:
                     try:
                         maxRes = functions_for_steps[step](model=model, optimParams=optimParams["{:s}_optim_params".format(step)])
                         message = "Iteration {:02d}, {:s} end: {:f}, niter: {:d}, nfeval: {:d}\n".format(iter, step, maxRes["lowerBound"], maxRes["niter"], maxRes["nfeval"])
-                    except:
+                    except Exception as e:
+                        ex_type, ex_value, ex_traceback = sys.exc_info()
+                        # Extract unformatter stack traces as tuples
+                        trace_back = traceback.extract_tb(ex_traceback)
+
+                        # Format stacktrace
+                        stack_trace = list()
+
+                        for trace in trace_back:
+                            stack_trace.append("File : %s , Line : %d, Func.Name : %s, Message : %s" % (trace[0], trace[1], trace[2], trace[3]))
+
+                        print("Exception type : %s " % ex_type.__name__)
+                        print("Exception message : %s" %ex_value)
+                        print("Stack trace : %s" %stack_trace)
+
                         terminationInfo = ErrorTerminationInfo("Error", sys.exc_info()[:2])
                         return lowerBoundHist, elapsedTimeHist, terminationInfo
                     if verbose:
