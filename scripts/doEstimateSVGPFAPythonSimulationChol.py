adcd5f3ca907787facaefb3e91210ede5acef847
Joaquin Rapela <joacorapela@gmail.com>
2021-03-15 12:56:19 +0000

    Added priors

--- scripts/doEstimateSVGPFAPythonSimulationChol.py
+++ scripts/doEstimateSVGPFAPythonSimulationChol.py
@@ -37,7 +37,7 @@ def main(argv):
     indPointsLocsKMSRegEpsilon = float(estInitConfig["control_variables"]["indPointsLocsKMSRegEpsilon"])
 
     optimParamsConfig = estInitConfig._sections["optim_params"]
-    optimMethod = optimParamsConfig["method"]
+    optimMethod = optimParamsConfig["em_method"]
     optimParams = {}
     optimParams["em_max_iter"] = int(optimParamsConfig["em_max_iter"])
     steps = ["estep", "mstep_embedding", "mstep_kernels", "mstep_indpointslocs"]
@@ -145,12 +145,75 @@ def main(argv):
         kernelParams = model.getKernelsParams()[0]
         return kernelParams
 
+    def embeddingParamsLogPrior(embeddingParams):
+        def cElementLogPrior(x, min=0.1, max=10.0):
+            uniform = torch.distributions.uniform.Uniform(min, max)
+            answer = uniform.log_prob(x)
+            return answer
+
+        def dElementLogPrior(x, mean=0.0, sd=10.0):
+            normal = torch.distributions.normal.Normal(loc=mean, scale=sd)
+            answer = normal.log_prob(x)
+            return answer
+
+        C = embeddingParams[0]
+        ClogProb = 0.0
+        for row in C:
+            for value in row:
+                ClogProb = ClogProb + cElementLogPrior(x=value)
+
+        d = embeddingParams[1]
+        dLogProb = 0.0
+        for value in d:
+                dLogProb = dLogProb + dElementLogPrior(x=value)
+
+        answer = ClogProb + dLogProb
+        return answer
+
+    def kernelsParamsLogPrior(kernelsParams):
+        def periodLogPrior(x, mean=5.0, sd=0.25):
+            normal = torch.distributions.normal.Normal(loc=mean, scale=sd)
+            answer = normal.log_prob(x)
+            return answer
+
+        def lengthscaleLogPrior(x, mean=2.25, sd=1.0):
+            normal = torch.distributions.normal.Normal(loc=mean, scale=sd)
+            answer = normal.log_prob(x)
+            return answer
+
+        lengthscale = kernelsParams[0][0]
+        lengthscaleLogProb = lengthscaleLogPrior(lengthscale)
+
+        period = kernelsParams[0][1]
+        periodLogProb = periodLogPrior(period)
+
+        answer = lengthscaleLogProb + periodLogProb
+        return answer
+
+    def indPointsLocsLogPrior(indPointsLocs):
+        def elementLogPrior(x):
+            uniform = torch.distributions.uniform.Uniform(0, torch.tensor(trialsLengths).max())
+            answer = uniform.log_prob(x)
+            return answer
+
+        indPointsLocsLogProb = 0.0
+        for value in indPointsLocs[0][0,:,0]:
+            indPointsLocsLogProb = indPointsLocsLogProb + elementLogPrior(value)
+        return indPointsLocsLogProb
+
+    paramsLogPriors = {
+        "embedding": embeddingParamsLogPrior,
+        "kernels": kernelsParamsLogPrior,
+        "indPointsLocs": indPointsLocsLogPrior,
+    }
+
     # create model
     model = stats.svGPFA.svGPFAModelFactory.SVGPFAModelFactory.buildModel(
         conditionalDist=stats.svGPFA.svGPFAModelFactory.PointProcess,
         linkFunction=stats.svGPFA.svGPFAModelFactory.ExponentialLink,
         embeddingType=stats.svGPFA.svGPFAModelFactory.LinearEmbedding,
-        kernels=kernels)
+        kernels=kernels,
+        paramsLogPriors=paramsLogPriors)
 
     model.setInitialParamsAndData(measurements=spikesTimes,
                                   initialParams=initialParams,
@@ -159,7 +222,7 @@ def main(argv):
 
     # maximize lower bound
     svEM = stats.svGPFA.svEM.SVEM()
-    lowerBoundHist, elapsedTimeHist, terminationInfo, iterationsModelParams  = svEM.maximize(model=model, optimParams=optimParams, method=optimMethod, getIterationModelParamsFn=getKernelParams)
+    lowerBoundHist, elapsedTimeHist, terminationInfo, iterationsModelParams = svEM.maximize(model=model, optimParams=optimParams, method=optimMethod, getIterationModelParamsFn=getKernelParams)
 
     # save estimated values
     estimResConfig = configparser.ConfigParser()
