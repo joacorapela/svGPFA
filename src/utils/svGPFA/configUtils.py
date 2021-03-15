d9e6f3b1d0feca102437c596be6d7f3d89c2bb04
Joaquin Rapela <joacorapela@gmail.com>
2021-03-15 19:39:43 +0000

    Added getParamsLogPriors

--- src/utils/svGPFA/configUtils.py
+++ src/utils/svGPFA/configUtils.py
@@ -5,6 +5,140 @@ import pandas as pd
 import torch
 import stats.kernels
 
+def getParamsLogPriors(nLatents, config):
+    embeddingLogPriors = _getEmbeddingLogPriors(config=config)
+    kernelsLogPriors = _getKernelsLogPriors(nLatents=nLatents, config=config)
+    indPointsLocsLogPriors = _getIndPointsLocsLogPriors(config=config)
+    answer = {
+        "embedding": embeddingLogPriors,
+        "kernels": kernelsLogPriors,
+        "indPointsLocs": indPointsLocsLogPriors
+    }
+    return answer
+
+def _getIndPointsLocsLogPriors(config):
+    def indPointLocsLogPrior(indPointsLocs, indPointsLocs_element_log_prior):
+        nLatents = len(indPointsLocs)
+        indPointsLocsLogProb = 0.0
+        for k in range(nLatents):
+            nTrials = indPointsLocs[k].shape[2]
+            for r in range(nTrials):
+                for indPointLoc in indPointsLocs[k][0,:,r]:
+                    indPointsLocsLogProb = indPointsLocsLogProb + indPointsLocs_element_log_prior(indPointLoc)
+        # pdb.set_trace()
+        return indPointsLocsLogProb
+
+    indPointsLocs_prior_type = config["indPoints_params"]["indPointsLocs_prior_type"]
+    if indPointsLocs_prior_type == "uniform":
+        indPointsLocs_prior_min = float(config["indPoints_params"]["indPointsLocs_prior_min"])
+        indPointsLocs_prior_max = float(config["indPoints_params"]["indPointsLocs_prior_max"])
+        def indPointsLocs_element_log_prior(x, min=indPointsLocs_prior_min, max=indPointsLocs_prior_max):
+            uniform = torch.distributions.uniform.Uniform(min, max)
+            answer = uniform.log_prob(x)
+            return answer
+    else:
+        raise NotImplementedError("Prior for type {:s} for the inducing points locations has not been implemented yet".format(C_prior_type))
+
+    answer = lambda indPointsLocs: indPointLocsLogPrior(indPointsLocs=indPointsLocs, indPointsLocs_element_log_prior=indPointsLocs_element_log_prior)
+    return answer
+
+
+def _getKernelsLogPriors(nLatents, config):
+    kernelsLogPriors = [None]*nLatents
+    for i in range(nLatents):
+        kernelsLogPriors[i] = _getKernelLogPrior(latent=i, config=config)
+    return kernelsLogPriors
+
+def _getKernelLogPrior(latent, config):
+    def kernelParamsLogPrior(kernelParams, kernelScales, lengthscaleLogPrior, periodLogPrior):
+        lengthscale = kernelParams[0]/kernelScales[0]
+        lengthscaleLogProb = lengthscaleLogPrior(lengthscale)
+
+        period = kernelParams[1]/kernelScales[1]
+        periodLogProb = periodLogPrior(period)
+
+        answer = lengthscaleLogProb + periodLogProb
+        # pdb.set_trace()
+        return answer
+
+    kernel_type = config["kernel_params"]["kTypeLatent{:d}".format(latent)]
+    if kernel_type == "periodic":
+        lengthscaleScale = float(config["kernel_params"]["kLengthscaleScaleLatent{:d}".format(latent)])
+        periodScale = float(config["kernel_params"]["kPeriodScaleLatent{:d}".format(latent)])
+        kernelScales = [lengthscaleScale, periodScale]
+
+        lengthscale_prior_type = config["kernel_params"]["kLengthscaleLatent{:d}_prior_type".format(latent)]
+        if lengthscale_prior_type=="normal":
+            lengthscale_prior_mean = float(config["kernel_params"]["kLengthscaleLatent{:d}_prior_mean".format(latent)])
+            lengthscale_prior_std = float(config["kernel_params"]["kLengthscaleLatent{:d}_prior_std".format(latent)])
+            def lengthscaleLogPrior(x, mean=lengthscale_prior_mean, std=lengthscale_prior_std):
+                normal = torch.distributions.normal.Normal(loc=mean, scale=std)
+                answer = normal.log_prob(x)
+                return answer
+        else:
+            raise NotImplementedError("Prior for type {:s} for lengthscale has not been implemented yet".format(lengthscale_prior_type))
+
+        period_prior_type = config["kernel_params"]["kPeriodLatent{:d}_prior_type".format(latent)]
+        if period_prior_type=="normal":
+            period_prior_mean = float(config["kernel_params"]["kPeriodLatent{:d}_prior_mean".format(latent)])
+            period_prior_std = float(config["kernel_params"]["kPeriodLatent{:d}_prior_std".format(latent)])
+            def periodLogPrior(x, mean=period_prior_mean, std=period_prior_std):
+                normal = torch.distributions.normal.Normal(loc=mean, scale=std)
+                answer = normal.log_prob(x)
+                # pdb.set_trace()
+                return answer
+        else:
+            raise NotImplementedError("Prior for type {:s} for period has not been implemented yet".format(lengthscale_prior_type))
+
+    else:
+        raise NotImplementedError("Prior for kernel {:s} has not been implemented yet".format(kernel_type))
+
+    answer = lambda kernelParams: kernelParamsLogPrior(kernelParams=kernelParams, kernelScales=kernelScales, lengthscaleLogPrior=lengthscaleLogPrior, periodLogPrior=periodLogPrior)
+    return answer
+
+
+def _getEmbeddingLogPriors(config):
+    def embeddingParamsLogPrior(embeddingParams, C_element_log_prior, d_element_log_prior):
+        C = embeddingParams[0]
+        ClogProb = 0.0
+        for row in C:
+            for value in row:
+                ClogProb = ClogProb + C_element_log_prior(x=value)
+
+        d = embeddingParams[1]
+        dLogProb = 0.0
+        for value in d:
+            dLogProb = dLogProb + d_element_log_prior(x=value)
+
+        answer = ClogProb + dLogProb
+        # pdb.set_trace()
+        return answer
+
+    C_prior_type = config["embedding_params"]["C_prior_type"]
+    if C_prior_type == "uniform":
+        C_prior_min = float(config["embedding_params"]["C_prior_min"])
+        C_prior_max = float(config["embedding_params"]["C_prior_max"])
+        def C_element_log_prior(x, min=C_prior_min, max=C_prior_max):
+            uniform = torch.distributions.uniform.Uniform(min, max)
+            answer = uniform.log_prob(x)
+            return answer
+    else:
+        raise NotImplementedError("Prior for type {:s} for embedding C matrix has not been implemented yet".format(C_prior_type))
+
+    d_prior_type = config["embedding_params"]["d_prior_type"]
+    if d_prior_type == "normal":
+        d_prior_mean = float(config["embedding_params"]["d_prior_mean"])
+        d_prior_std = float(config["embedding_params"]["d_prior_std"])
+        def d_element_log_prior(x, mean=d_prior_mean, std=d_prior_std):
+            normal = torch.distributions.normal.Normal(mean, std)
+            answer = normal.log_prob(x)
+            return answer
+    else:
+        raise NotImplementedError("Prior for type {:s} for embedding d vector has not been implemented yet".format(C_prior_type))
+
+    answer = lambda embeddingParams: embeddingParamsLogPrior(embeddingParams=embeddingParams, C_element_log_prior=C_element_log_prior, d_element_log_prior=d_element_log_prior)
+    return answer
+
 def getVariationalMean0(nLatents, nTrials, config, keyNamePattern="qMu0Latent{:d}Trial{:d}_filename"):
     qMu0 = [[] for r in range(nLatents)]
     for k in range(nLatents):
