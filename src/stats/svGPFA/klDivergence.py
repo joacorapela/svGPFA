feeabc62bac38444198cc68cf739cacc6ed4cbee
Joaquin Rapela <joacorapela@gmail.com>
2021-12-03 21:09:55 +0000

    Kzz inverses are now calculated by indPointsLocsKMS

--- src/stats/svGPFA/klDivergence.py
+++ src/stats/svGPFA/klDivergence.py
@@ -12,25 +12,27 @@ class KLDivergence:
     def evalSumAcrossLatentsAndTrials(self):
         klDiv = 0
         qSigma = self._svPosteriorOnIndPoints.buildQSigma()
-        for k in range(len(self._indPointsLocsKMS.getKzzChol())):
+        nLatents = len(qSigma)
+        for k in range(nLatents):
             klDivK = self._evalSumAcrossTrials(
                 Kzz=self._indPointsLocsKMS.getKzz()[k],
-                KzzChol=self._indPointsLocsKMS.getKzzChol()[k],
                 qMu=self._svPosteriorOnIndPoints.getQMu()[k],
-                qSigma=qSigma[k])
+                qSigma=qSigma[k],
+                latentIndex=k)
             klDiv += klDivK
         return klDiv
 
-    def _evalSumAcrossTrials(self, Kzz, KzzChol, qMu, qSigma):
+    def _evalSumAcrossTrials(self, Kzz, qMu, qSigma, latentIndex):
         # ESS \in nTrials x nInd x nInd
         ESS = qSigma + torch.matmul(qMu, qMu.permute(0,2,1))
         nTrials = qMu.shape[0]
         answer = 0
-        for trial in range(nTrials):
-            _, logdetKzz = Kzz[trial,:,:].slogdet() # O(n^3)
-            _, logdetQSigma = qSigma[trial,:,:].slogdet() # O(n^3)
-            traceTerm = torch.trace(
-                torch.cholesky_solve(ESS[trial,:,:], KzzChol[trial,:,:]))
+        for trialIndex in range(nTrials):
+            _, logdetKzz = Kzz[trialIndex,:,:].slogdet() # O(n^3)
+            _, logdetQSigma = qSigma[trialIndex,:,:].slogdet() # O(n^3)
+            # traceTerm = torch.trace(torch.cholesky_solve(ESS[trialIndex,:,:],
+            # KzzInv[trialIndex,:,:]))
+            traceTerm = torch.trace(self._indPointsLocsKMS.solveForLatentAndTrial(ESS[trialIndex,:,:], latentIndex=latentIndex, trialIndex=trialIndex))
             trialKL = .5*(traceTerm+logdetKzz-logdetQSigma-ESS.shape[1])
             answer += trialKL
         return answer
