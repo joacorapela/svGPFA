6afaa3b881e428cbe7f8b6cf8b8cb5d0a6f1c2d3
Joaquin Rapela <joacorapela@gmail.com>
2021-12-04 19:48:54 +0000

    Moved getDiagIndicesIn3DArray buildQSigmaFromQSVecAndQSDiag getSRQSigmaVec to miscUtils.py; removed commented getIndPointsCol0

--- src/utils/svGPFA/initUtils.py
+++ src/utils/svGPFA/initUtils.py
@@ -42,80 +42,6 @@ def getScaledIdentityQSigma0(scale, nTrials, nIndPointsPerLatent):
             qSigma0[k][r,:,:] = scale*torch.eye(nIndPointsPerLatent[k], dtype=torch.double)
     return qSigma0
 
-def getDiagIndicesIn3DArray(N, M, device=torch.device("cpu")):
-    frameDiagIndices = torch.arange(end=N, device=device)*(N+1)
-    frameStartIndices = torch.arange(end=M, device=device)*N**2
-    # torch way of computing an outer sum
-    diagIndices = (frameDiagIndices.reshape(-1,1)+frameStartIndices).flatten()
-    answer, _ = diagIndices.sort()
-    return answer
-
-def build3DdiagFromDiagVector(v, N, M):
-    assert(len(v)==N*M)
-    diagIndices = getDiagIndicesIn3DArray(N=N, M=M)
-    D = torch.zeros(M*N*N, dtype=v.dtype, device=v.device)
-    D[diagIndices] = v
-    reshapedD = D.reshape(shape = (M, N, N))
-    return reshapedD
-
-def buildQSigmaFromQSVecAndQSDiag(qSVec, qSDiag):
-    nLatents = len(qSVec)
-    nTrials = qSVec[0].shape[0]
-    qSigma = [[None] for k in range(nLatents)]
-    for k in range(nLatents):
-        nIndK = qSDiag[k].shape[1]
-        # qq \in nTrials x nInd[k] x 1
-        qq = qSVec[k].reshape(shape=(nTrials, nIndK, 1))
-        # dd \in nTrials x nInd[k] x 1
-        nIndKVarRnkK = qSVec[k].shape[1]
-        dd = build3DdiagFromDiagVector(v=(qSDiag[k].flatten())**2, M=nTrials, N=nIndKVarRnkK)
-        # qSigma[k] \in nTrials x nInd[k] x nInd[k]
-        qSigma[k] = torch.matmul(qq, torch.transpose(a=qq, dim0=1, dim1=2)) + dd
-    return(qSigma)
-
-def getSRQSigmaVecsFromSRMatrices(srMatrices):
-    nLatents = len(srMatrices)
-    nTrials = srMatrices[0].shape[0]
-
-    srQSigmaVec = [[None] for k in range(nLatents)]
-    for k in range(nLatents):
-        nIndPointsK = srMatrices[k].shape[1]
-        Pk = int((nIndPointsK+1)*nIndPointsK/2)
-        srQSigmaVec[k] = torch.empty((nTrials, Pk, 1), dtype=torch.double)
-        for r in range(nTrials):
-            cholKR = srMatrices[k][r,:,:]
-            trilIndices = torch.tril_indices(nIndPointsK, nIndPointsK)
-            cholKRVec = cholKR[trilIndices[0,:], trilIndices[1,:]]
-            srQSigmaVec[k][r,:,0] = cholKRVec
-    return srQSigmaVec
-
-def getSRQSigmaVec(qSVec, qSDiag):
-    nLatents = len(qSVec)
-    nTrials = qSVec[0].shape[0]
-    qSigma = buildQSigmaFromQSVecAndQSDiag(qSVec=qSVec, qSDiag=qSDiag)
-    srQSigmaVec = [[None] for k in range(nLatents)]
-    for k in range(nLatents):
-        nIndPointsK = qSigma[k].shape[1]
-        Pk = int((nIndPointsK+1)*nIndPointsK/2)
-        srQSigmaVec[k] = torch.empty((nTrials, Pk, 1), dtype=torch.double)
-        for r in range(nTrials):
-            cholKR = torch.cholesky(qSigma[k][r,:,:])
-            trilIndices = torch.tril_indices(nIndPointsK, nIndPointsK)
-            cholKRVec = cholKR[trilIndices[0,:], trilIndices[1,:]]
-            srQSigmaVec[k][r,:,0] = cholKRVec
-    return srQSigmaVec
-
-# def getIndPointLocs0(nIndPointsPerLatent, trialsLengths, firstIndPointLoc):
-#     nLatents = len(nIndPointsPerLatent)
-#     nTrials = len(trialsLengths)
-#
-#     Z0 = [[] for k in range(nLatents)]
-#     for k in range(nLatents):
-#         Z0[k] = torch.empty((nTrials, nIndPointsPerLatent[k], 1), dtype=torch.double)
-#         for r in range(nTrials):
-#             Z0[k][r,:,0] = torch.linspace(firstIndPointLoc, trialsLengths[r], nIndPointsPerLatent[k])
-#     return Z0
-
 def getSVPosteriorOnIndPointsParams0(nIndPointsPerLatent, nLatents, nTrials, scale):
     qMu0 = [[] for k in range(nLatents)]
     qSVec0 = [[] for k in range(nLatents)]
@@ -142,4 +68,20 @@ def getKernelsScaledParams0(kernels, noiseSTD):
         trueParams = kernels[k].getScaledParams()
         kernelsParams0[k] = noiseSTD*torch.randn(len(trueParams))+trueParams
     return kernelsParams0
+
+def getSRQSigmaVecsFromSRMatrices(srMatrices):
+    nLatents = len(srMatrices)
+    nTrials = srMatrices[0].shape[0]
+
+    srQSigmaVec = [[None] for k in range(nLatents)]
+    for k in range(nLatents):
+        nIndPointsK = srMatrices[k].shape[1]
+        Pk = int((nIndPointsK+1)*nIndPointsK/2)
+        srQSigmaVec[k] = torch.empty((nTrials, Pk, 1), dtype=torch.double)
+        for r in range(nTrials):
+            cholKR = srMatrices[k][r,:,:]
+            trilIndices = torch.tril_indices(nIndPointsK, nIndPointsK)
+            cholKRVec = cholKR[trilIndices[0,:], trilIndices[1,:]]
+            srQSigmaVec[k][r,:,0] = cholKRVec
+    return srQSigmaVec
 
