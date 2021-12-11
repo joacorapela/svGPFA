6659c5a128ebb72850978a9c27dcf084ef2ac55f
Joaquin Rapela <joacorapela@gmail.com>
2021-12-11 14:40:30 +0000

    Updated to allow SciPy optimizations

--- src/stats/svGPFA/svLowerBound.py
+++ src/stats/svGPFA/svLowerBound.py
@@ -22,9 +22,13 @@ class SVLowerBound:
         eLLEval = self._eLL.evalSumAcrossTrialsAndNeurons()
         klDivEval = self._klDiv.evalSumAcrossLatentsAndTrials()
         theEval = eLLEval-klDivEval
+#         if torch.abs(theEval)>1e30:
+#             import pdb; pdb.set_trace()
         if torch.isinf(theEval):
             # raise RuntimeError("infinity lower bound detected")
             warnings.warn("infinity lower bound detected")
+        if torch.isnan(theEval):
+            import pdb; pdb.set_trace()
         return theEval
 
 #     def sampleCIFs(self, times):
@@ -78,15 +82,85 @@ class SVLowerBound:
     def getSVEmbeddingParams(self):
         return self._eLL.getSVEmbeddingParams()
 
-    def getIndPointsLocs(self):
-        return self._eLL.getIndPointsLocs()
-
     def getKernelsParams(self):
         return self._eLL.getKernelsParams()
+
+    def getIndPointsLocs(self):
+        return self._eLL.getIndPointsLocs()
 
     def predictLatents(self, newTimes):
         return self._eLL.predictLatents(newTimes=newTimes)
 
     def predictEmbedding(self, newTimes):
         return self._eLL.predictEmbedding(newTimes=newTimes)
+
+
+class SVLowerBoundWithParamsGettersAndSetters(SVLowerBound):
+
+    def __init__(self, eLL, klDiv):
+        super(SVLowerBoundWithParamsGettersAndSetters, self).__init__(eLL, klDiv)
+        # shortcuts
+        self._svPosteriorOnIndPoints = klDiv.get_svPosteriorOnIndPoints()
+        self._svEmbedding = eLL.get_svEmbeddingAllTimes()
+        self._indPointsLocsKMS = klDiv.get_indPointsLocsKMS()
+
+    # svPosterioOnIndPoints
+    def get_flattened_svPosteriorOnIndPoints_params(self):
+        flattened_params = self._svPosteriorOnIndPoints.get_flattened_params()
+        return flattened_params
+
+    def get_flattened_svPosteriorOnIndPoints_params_grad(self):
+        flattened_params_grad = self._svPosteriorOnIndPoints.get_flattened_params_grad()
+        return flattened_params_grad
+
+    def set_svPosteriorOnIndPoints_params_from_flattened(self, flattened_params):
+        self._svPosteriorOnIndPoints.set_params_from_flattened(flattened_params=flattened_params)
+
+    def set_svPosteriorOnIndPoints_params_requires_grad(self, requires_grad):
+        self._svPosteriorOnIndPoints.set_params_requires_grad(requires_grad=requires_grad)
+
+    # svEmbedding
+    def get_flattened_svEmbedding_params(self):
+        flattened_params = self._svEmbedding.get_flattened_params()
+        return flattened_params
+
+    def get_flattened_svEmbedding_params_grad(self):
+        flattened_params_grad = self._svEmbedding.get_flattened_params_grad()
+        return flattened_params_grad
+
+    def set_svEmbedding_params_from_flattened(self, flattened_params):
+        self._svEmbedding.set_params_from_flattened(flattened_params=flattened_params)
+
+    def set_svEmbedding_params_requires_grad(self, requires_grad):
+        self._svEmbedding.set_params_requires_grad(requires_grad=requires_grad)
+
+    # kernels_params
+    def get_flattened_kernels_params(self):
+        flattened_params = self._indPointsLocsKMS.get_flattened_kernels_params()
+        return flattened_params
+
+    def get_flattened_kernels_params_grad(self):
+        flattened_params_grad = self._indPointsLocsKMS.get_flattened_kernels_params_grad()
+        return flattened_params_grad
+
+    def set_kernels_params_from_flattened(self, flattened_params):
+        self._indPointsLocsKMS.set_kernels_params_from_flattened(flattened_params=flattened_params)
+
+    def set_kernels_params_requires_grad(self, requires_grad):
+        self._indPointsLocsKMS.set_kernels_params_requires_grad(requires_grad=requires_grad)
+
+    # indPointsLocs
+    def get_flattened_indPointsLocs(self):
+        flattened_params = self._indPointsLocsKMS.get_flattened_indPointsLocs()
+        return flattened_params
+
+    def get_flattened_indPointsLocs_grad(self):
+        flattened_params_grad = self._indPointsLocsKMS.get_flattened_indPointsLocs_grad()
+        return flattened_params_grad
+
+    def set_indPointsLocs_from_flattened(self, flattened_params):
+        self._indPointsLocsKMS.set_indPointsLocs_from_flattened(flattened_params=flattened_params)
+
+    def set_indPointsLocs_requires_grad(self, requires_grad):
+        self._indPointsLocsKMS.set_indPointsLocs_requires_grad(requires_grad=requires_grad)
 
