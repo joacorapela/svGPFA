3a9aa108f0ab723182c0b2a885fef02753d6549b
Joaquin Rapela <joacorapela@gmail.com>
2021-12-11 14:43:23 +0000

    Updated to allow SciPy optimizations

--- src/stats/svGPFA/svGPFAModelFactory.py
+++ src/stats/svGPFA/svGPFAModelFactory.py
@@ -36,14 +36,14 @@ indPointsCovChol = 100001
 class SVGPFAModelFactory:
 
     @staticmethod
-    def buildModel(conditionalDist, linkFunction, embeddingType, kernels,
+    def buildModelPytorch(conditionalDist, linkFunction, embeddingType, kernels,
                    kernelMatrixInvMethod, indPointsCovRep):
 
         if conditionalDist==PointProcess:
             if embeddingType==LinearEmbedding:
                 if linkFunction==ExponentialLink:
                     if indPointsCovRep==indPointsCovChol:
-                        qU = stats.svGPFA.svPosteriorOnIndPoints.SVPosteriorOnIndPoints()
+                        qU = stats.svGPFA.svPosteriorOnIndPoints.SVPosteriorOnIndPointsChol()
                     elif indPointsCovRep==indPointsCovRank1PlusDiag:
                         qU = stats.svGPFA.svPosteriorOnIndPoints.SVPosteriorOnIndPointsRank1PlusDiag()
                     else:
@@ -109,3 +109,79 @@ class SVGPFAModelFactory:
                              repr(conditionalDist))
 
         return svlb
+
+    @staticmethod
+    def buildModelSciPy(conditionalDist, linkFunction, embeddingType, kernels,
+                        kernelMatrixInvMethod, indPointsCovRep):
+
+        if conditionalDist==PointProcess:
+            if embeddingType==LinearEmbedding:
+                if linkFunction==ExponentialLink:
+                    if indPointsCovRep==indPointsCovChol:
+                        qU = stats.svGPFA.svPosteriorOnIndPoints.SVPosteriorOnIndPointsCholWithGettersAndSetters()
+                    elif indPointsCovRep==indPointsCovRank1PlusDiag:
+                        qU = stats.svGPFA.svPosteriorOnIndPoints.SVPosteriorOnIndPointsRank1PlusDiagWithGettersAndSetters()
+                    else:
+                        raise ValueError("Invalid indPointsCovRep")
+                    if kernelMatrixInvMethod==kernelMatrixInvChol:
+                        indPointsLocsKMS = stats.svGPFA.kernelsMatricesStore.IndPointsLocsKMS_CholWithGettersAndSetters()
+                    elif kernelMatrixInvMethod==kernelMatrixInvPInv:
+                        indPointsLocsKMS = stats.svGPFA.kernelsMatricesStore.IndPointsLocsKMS_PInvWithGettersAndSetters()
+                    else:
+                        raise ValueError("Invalid kernelMatrixInvMethod")
+                    indPointsLocsAndAllTimesKMS = stats.svGPFA.kernelsMatricesStore.IndPointsLocsAndAllTimesKMS()
+                    indPointsLocsAndAssocTimesKMS = stats.svGPFA.kernelsMatricesStore.IndPointsLocsAndAssocTimesKMS()
+                    qKAllTimes = stats.svGPFA.svPosteriorOnLatents.SVPosteriorOnLatentsAllTimes(
+                        svPosteriorOnIndPoints=qU,
+                        indPointsLocsKMS=indPointsLocsKMS,
+                        indPointsLocsAndTimesKMS=indPointsLocsAndAllTimesKMS)
+                    qKAssocTimes = stats.svGPFA.svPosteriorOnLatents.SVPosteriorOnLatentsAssocTimes(
+                        svPosteriorOnIndPoints=qU,
+                        indPointsLocsKMS=indPointsLocsKMS,
+                        indPointsLocsAndTimesKMS=indPointsLocsAndAssocTimesKMS)
+                    qHAllTimes = stats.svGPFA.svEmbedding.LinearSVEmbeddingAllTimesWithParamsGettersAndSetters(
+                        svPosteriorOnLatents=qKAllTimes)
+                    qHAssocTimes = stats.svGPFA.svEmbedding.LinearSVEmbeddingAssocTimes(
+                        svPosteriorOnLatents=qKAssocTimes)
+                    eLL = stats.svGPFA.expectedLogLikelihood.PointProcessELLExpLink(
+                        svEmbeddingAllTimes=qHAllTimes,
+                        svEmbeddingAssocTimes=qHAssocTimes)
+                    klDiv = stats.svGPFA.klDivergence.KLDivergence(indPointsLocsKMS=indPointsLocsKMS,
+                                         svPosteriorOnIndPoints=qU)
+                    svlb = stats.svGPFA.svLowerBound.SVLowerBoundWithParamsGettersAndSetters(eLL=eLL, klDiv=klDiv)
+                    svlb.setKernels(kernels=kernels)
+                else:
+                    raise ValueError("Invalid linkFunction=%s"%
+                                     repr(linkFunction))
+            else:
+                raise ValueError("Invalid embeddingType=%s"%
+                                 repr(embeddingType))
+        elif conditionalDist==Poisson:
+            if embeddingType==LinearEmbedding:
+                if linkFunction==ExponentialLink:
+                    qU = stats.svGPFA.svPosteriorOnIndPoints.SVPosteriorOnIndPoints()
+                    if kernelMatrixInvMethod==Cholesky:
+                        indPointsLocsKMS = stats.svGPFA.kernelsMatricesStore.IndPointsLocsKMS_Chol()
+                    elif kernelMatrixInvMethod==PInv:
+                        indPointsLocsKMS = stats.svGPFA.kernelsMatricesStore.IndPointsLocsKMS_Pinv()
+                    else:
+                        raise ValueError("Invalid kernelMatrixInvMethod")
+                    indPointsLocsAndAllTimesKMS = stats.svGPFA.kernelsMatricesStore.IndPointsLocsAndAllTimesKMS()
+                    qKAllTimes = stats.svGPFA.svPosteriorOnLatents.SVPosteriorOnLatentsAllTimes(
+                        svPosteriorOnIndPoints=qU,
+                        indPointsLocsKMS=indPointsLocsKMS,
+                        indPointsLocsAndTimesKMS=indPointsLocsAndAllTimesKMS)
+                    qHAllTimes = stats.svGPFA.svEmbedding.LinearSVEmbeddingAllTimes(
+                        svPosteriorOnLatents=qKAllTimes)
+                    eLL = stats.svGPFA.expectedLogLikelihood.PoissonELLExpLink(
+                        svEmbeddingAllTimes=qHAllTimes)
+                    klDiv = stats.svGPFA.klDivergence.KLDivergence(indPointsLocsKMS=indPointsLocsKMS,
+                                         svPosteriorOnIndPoints=qU)
+                    svlb = stats.svGPFA.svLowerBound.SVLowerBound(eLL=eLL, klDiv=klDiv)
+                    svlb.setKernels(kernels=kernels)
+        else:
+            raise ValueError("Invalid conditionalDist=%s"%
+                             repr(conditionalDist))
+
+        return svlb
+
