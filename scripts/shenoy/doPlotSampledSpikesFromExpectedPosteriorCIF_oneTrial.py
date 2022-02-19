f3739648e54554d7a4121328cf5bfb8f94c8ee24
Joaquin Rapela <joacorapela@gmail.com>
2022-02-19 21:13:00 +0000

    Simple change: trial to trial_index

--- scripts/shenoy/doPlotSampledSpikesFromExpectedPosteriorCIF_oneTrial.py
+++ scripts/shenoy/doPlotSampledSpikesFromExpectedPosteriorCIF_oneTrial.py
@@ -15,7 +15,7 @@ def main(argv):
                         help="filename pattern for spikes sampled from the expected posterior CIF",
                         default="results/{:08d}_spikesFromExpectedPosteriorCIF.pickle")
     parser.add_argument("--data_filename", help="data filename", default="/nfs/ghome/live/rapela/dev/research/gatsby-swc/datasets/george20040123_hnlds.mat")
-    parser.add_argument("--trial", help="trial", type=int, default=0)
+    parser.add_argument("--trial_index", help="trial index", type=int, default=0)
     parser.add_argument("--location", help="location", type=int, default=0)
     parser.add_argument("--title_pattern", help="title pattern", default="Trial {:d}")
     parser.add_argument("--target_onset_colour", help="target onset colour", default="violet")
@@ -31,7 +31,7 @@ def main(argv):
     estResNumber = args.estResNumber
     spikesFilenamePattern = args.spikesFilenamePattern
     data_filename = args.data_filename
-    trial = args.trial
+    trial_index = args.trial_index
     location = args.location
     title_pattern = args.title_pattern
     target_onset_colour = args.target_onset_colour
@@ -53,21 +53,21 @@ def main(argv):
 #                                                               location=location)
     spikesFilename = spikesFilenamePattern.format(estResNumber)
     with open(spikesFilename, "rb") as f: loadRes = pickle.load(f)
-    spikes_times = loadRes["spikesTimes"][trial]
-    time_fix_target = mat["Rb"][trial,location]["timeFixTarget"].squeeze()
-    time_go_cue = mat["Rb"][trial,location]["timeGoCue"].squeeze()
-    time_move_onset = mat["Rb"][trial,location]["timeMoveOnset"].squeeze()
-    time_move_end = mat["Rb"][trial,location]["timeMoveEnd"].squeeze()
-    title = title_pattern.format(trial, location)
+    spikes_times = loadRes["spikesTimes"][trial_index]
+    time_fix_target = mat["Rb"][trial_index,location]["timeFixTarget"].squeeze()
+    time_go_cue = mat["Rb"][trial_index,location]["timeGoCue"].squeeze()
+    time_move_onset = mat["Rb"][trial_index,location]["timeMoveOnset"].squeeze()
+    time_move_end = mat["Rb"][trial_index,location]["timeMoveEnd"].squeeze()
+    title = title_pattern.format(trial_index, location)
     fig = plot.svGPFA.plotUtilsPlotly.getSpikesTimesPlotOneTrial(
-        spikesTimes=spikes_times, title=title, xlabel=xlabel)
+        spikes_times=spikes_times, title=title, xlabel=xlabel)
     fig.add_vline(x=time_fix_target, line_width=vline_width, line_dash=vline_style, line_color=target_onset_colour)
     fig.add_vline(x=time_go_cue, line_width=vline_width, line_dash=vline_style, line_color=go_cue_colour)
     fig.add_vline(x=time_move_onset, line_width=vline_width, line_dash=vline_style, line_color=move_onset_colour)
     fig.add_vline(x=time_move_end, line_width=vline_width, line_dash=vline_style, line_color=move_end_colour)
-    png_fig_filename = fig_filename_pattern.format(estResNumber, trial, "png")
+    png_fig_filename = fig_filename_pattern.format(estResNumber, trial_index, "png")
     fig.write_image(png_fig_filename)
-    html_fig_filename = fig_filename_pattern.format(estResNumber, trial, "html")
+    html_fig_filename = fig_filename_pattern.format(estResNumber, trial_index, "html")
     fig.write_html(html_fig_filename)
 
     # fig.show()
