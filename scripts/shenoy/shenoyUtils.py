5a561fa497775e46424964c0772272f3693fe20e
joacorapela <joacorapela@gmail.com>
2022-03-13 22:03:38 +0000

    Moved several functions to utils.neuralDataAnalysis

--- scripts/shenoy/shenoyUtils.py
+++ scripts/shenoy/shenoyUtils.py
@@ -23,90 +23,6 @@ def getTrialsAndLocationSpikesTimes(mat, trials_indices, location):
         spikesTimes[i] = trial_spikes_times
     return spikesTimes
 
-def clipSpikesTimes(spikes_times, from_time, to_time):
-    nTrials = len(spikes_times)
-    clipped_spikes_times = [[]] * nTrials
-    for r in range(nTrials):
-        clipped_spikes_times[r] = clipTrialSpikesTimes(trial_spikes_times=spikes_times[r],
-                                                       from_time=from_time,
-                                                       to_time=to_time)
-    return clipped_spikes_times
-
-def clipTrialSpikesTimes(trial_spikes_times, from_time, to_time):
-    nNeurons = len(trial_spikes_times)
-    clipped_trial_spikes_times = [[]] * nNeurons
-    for n in range(nNeurons):
-        clipped_trial_spikes_times[n] = clipNeuronSpikesTimes(
-            neuron_spikes_times=trial_spikes_times[n],
-            from_time=from_time, to_time=to_time)
-    return clipped_trial_spikes_times
-
-def clipNeuronSpikesTimes(neuron_spikes_times, from_time, to_time):
-    clipped_neuron_spikes_times = neuron_spikes_times[torch.logical_and(
-        from_time <= neuron_spikes_times, neuron_spikes_times < to_time)]
-    return clipped_neuron_spikes_times
-
-def offsetSpikeTimes(spikes_times, offset):
-    nTrials = len(spikes_times)
-    offsetted_spikes_times = [[]] * nTrials
-    for r in range(nTrials):
-        offsetted_spikes_times[r] = offsetTrialSpikesTimes(trial_spikes_times=spikes_times[r], offset=offset)
-    return offsetted_spikes_times
-
-def offsetTrialSpikesTimes(trial_spikes_times, offset):
-    nNeurons = len(trial_spikes_times)
-    offsetted_trial_spikes_times = [[]] * nNeurons
-    for n in range(nNeurons):
-        offsetted_trial_spikes_times[n] = trial_spikes_times[n]+offset
-    return offsetted_trial_spikes_times
-
-def removeUnits(spikes_times, units_to_remove):
-    nTrials = len(spikes_times)
-    spikes_times_woUnits = [[]] * nTrials
-    for r in range(nTrials):
-        spikes_times_woUnits[r] = \
-                removeUnitsFromTrial(trial_spikes_times=spikes_times[r],
-                                     units_to_remove=units_to_remove)
-    return spikes_times_woUnits
-
-def removeUnitsFromTrial(trial_spikes_times, units_to_remove):
-    nNeurons = len(trial_spikes_times)
-    spikes_times_woUnits = []
-    for n in range(nNeurons):
-        if n not in units_to_remove:
-            spikes_times_woUnits.append(trial_spikes_times[n])
-    return spikes_times_woUnits
-
-def selectUnitsWithLessSpikesThanThrInAllTrials(spikes_times, thr):
-    nTrials = len(spikes_times)
-    nNeurons = len(spikes_times[0])
-    selected_units = set([i for i in range(nNeurons)])
-    for r in range(nTrials):
-        selected_trial_units = selectUnitsWithLessSpikesThanThrInTrial(
-            spikes_times=spikes_times[r], thr=thr)
-        selected_units = selected_units.intersection(selected_trial_units)
-    answer = list(selected_units)
-    return answer
-
-def selectUnitsWithLessSpikesThanThrInAnyTrial(spikes_times, thr):
-    nTrials = len(spikes_times)
-    selected_units = set([])
-    for r in range(nTrials):
-        selected_trial_units = selectUnitsWithLessSpikesThanThrInTrial(
-            spikes_times=spikes_times[r], thr=thr)
-        selected_units = selected_units.union(selected_trial_units)
-    answer = list(selected_units)
-    return answer
-    return selected_units
-
-def selectUnitsWithLessSpikesThanThrInTrial(spikes_times, thr):
-    nNeurons = len(spikes_times)
-    selected_units = set([])
-    for n in range(nNeurons):
-        if len(spikes_times[n])<thr:
-            selected_units.add(n)
-    return selected_units
-
 def getSpikesTimes(data_filename, trials_indices, location, from_time, to_time, min_nSpikes_perNeuron_perTrial):
     mat = scipy.io.loadmat(os.path.expanduser(data_filename))
     spikes_times = getTrialsAndLocationSpikesTimes(mat=mat,
@@ -115,17 +31,5 @@ def getSpikesTimes(data_filename, trials_indices, location, from_time, to_time, 
     spikes_times = clipSpikesTimes(spikes_times=spikes_times,
                                                from_time=from_time,
                                                to_time=to_time)
-    # spikes_times = offsetSpikeTimes(spikes_times=spikes_times, offset=-from_time)
-
-    # units_to_remove = selectUnitsWithLessSpikesThanThrInAnyTrial(
-    #     spikes_times=spikes_times, thr=min_nSpikes_perNeuron_perTrial)
-    nNeurons = len(spikes_times[0])
-    neurons_indices = [n for n in range(nNeurons)]
-    units_to_remove = \
-            selectUnitsWithLessSpikesThanThrInAllTrials(
-                spikes_times=spikes_times, thr=min_nSpikes_perNeuron_perTrial)
-    spikes_times = removeUnits(spikes_times=spikes_times,
-                                          units_to_remove=units_to_remove)
-    neurons_indices = [n for n in neurons_indices if n not in units_to_remove]
-    return spikes_times, neurons_indices
+    return spikes_times
 
