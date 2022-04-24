7496397ea3ec0cc50a6c25437603b2a68c1a53ac
Joaquin Rapela <joacorapela@gmail.com>
2022-04-24 16:31:29 +0100

    Changed imports to reflect the package restructure

--- projects/svGPFA_simulations/scripts/doPlotSVGPFAEstimates.py
+++ projects/svGPFA_simulations/scripts/doPlotSVGPFAEstimates.py
@@ -13,16 +13,12 @@ import pandas as pd
 import sklearn.metrics
 # import statsmodels.tsa.stattools
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-sys.path.append("../src")
-import stats.pointProcess.tests
-import utils.svGPFA.configUtils
-import utils.svGPFA.initUtils
-import utils.svGPFA.miscUtils
-import plot.svGPFA.plotUtils
-import plot.svGPFA.plotUtilsPlotly
+import gcnu_common.stats.point_processes.tests
+import svGPFA.utils.configUtils
+import svGPFA.utils.initUtils
+import svGPFA.utils.miscUtils
+# import svGPFA.plot.plotUtils
+import svGPFA.plot.plotUtilsPlotly
 
 def main(argv):
     parser = argparse.ArgumentParser()
@@ -41,25 +37,25 @@ def main(argv):
     ksTestGamma = args.ksTestGamma
     nTestPoints = args.nTestPoints
 
-    estimResMetaDataFilename = "results/{:08d}_estimation_metaData.ini".format(estResNumber)
-    modelSaveFilename = "results/{:08d}_estimatedModel.pickle".format(estResNumber)
-    lowerBoundHistVsIterNoFigFilenamePattern = "figures/{:08d}_lowerBoundHistVsIterNo.{{:s}}".format(estResNumber)
-    lowerBoundHistVsElapsedTimeFigFilenamePattern = "figures/{:08d}_lowerBoundHistVsElapsedTime.{{:s}}".format(estResNumber)
-    latentsFigFilenamePattern = "figures/{:08d}_estimatedLatent_trial{:03d}_neuron{:03d}.{{:s}}".format(estResNumber, trialToPlot, neuronToPlot)
-    ksTestTimeRescalingNumericalCorrectionFigFilename = "figures/{:08d}_ksTestTimeRescaling_numericalCorrection_trial{:03d}_neuron{:03d}.png".format(estResNumber, trialToPlot, neuronToPlot)
-    trueAndEstimatedCIFsFigFilenamePattern = "figures/{:08d}_trueAndEstimatedCIFs_trial{:03d}_neuron{:03d}.{{:s}}".format(estResNumber, trialToPlot, neuronToPlot)
-    rocFigFilename = "figures/{:08d}_rocAnalysis_trial{:03d}_neuron{:03d}.png".format(estResNumber, trialToPlot, neuronToPlot)
-    kernelsParamsFigFilenamePattern = "figures/{:08d}_trueAndEstimatedKernelsParams.{{:s}}".format(estResNumber)
-    embeddingParamsFigFilenamePattern = "figures/{:08d}_trueAndEstimatedEmbeddingParams.{{:s}}".format(estResNumber)
-    ksTestTimeRescalingAnalyticalCorrectionFigFilename = "figures/{:08d}_ksTestTimeRescaling_analyticalCorrection_trial{:03d}_neuron{:03d}.png".format(estResNumber, trialToPlot, neuronToPlot)
-    timeRescalingDiffCDFsFigFilename = "figures/{:08d}_timeRescalingDiffCDFs_analyticalCorrection_trial{:03d}_neuron{:03d}.png".format(estResNumber, trialToPlot, neuronToPlot)
-    timeRescaling1LagScatterPlotFigFilename = "figures/{:08d}_timeRescaling1LagScatterPlot_analyticalCorrection_trial{:03d}_neuron{:03d}.png".format(estResNumber, trialToPlot, neuronToPlot)
-    timeRescalingACFFigFilename = "figures/{:08d}_timeRescalingACF_analyticalCorrection_trial{:03d}_neuron{:03d}.png".format(estResNumber, trialToPlot, neuronToPlot)
+    estimResMetaDataFilename = "../results/{:08d}_estimation_metaData.ini".format(estResNumber)
+    modelSaveFilename = "../results/{:08d}_estimatedModel.pickle".format(estResNumber)
+    lowerBoundHistVsIterNoFigFilenamePattern = "../figures/{:08d}_lowerBoundHistVsIterNo.{{:s}}".format(estResNumber)
+    lowerBoundHistVsElapsedTimeFigFilenamePattern = "../figures/{:08d}_lowerBoundHistVsElapsedTime.{{:s}}".format(estResNumber)
+    latentsFigFilenamePattern = "../figures/{:08d}_estimatedLatent_trial{:03d}_neuron{:03d}.{{:s}}".format(estResNumber, trialToPlot, neuronToPlot)
+    ksTestTimeRescalingNumericalCorrectionFigFilenamePattern = "../figures/{:08d}_ksTestTimeRescaling_numericalCorrection_trial{:03d}_neuron{:03d}.{{:s}}".format(estResNumber, trialToPlot, neuronToPlot)
+    trueAndEstimatedCIFsFigFilenamePattern = "../figures/{:08d}_trueAndEstimatedCIFs_trial{:03d}_neuron{:03d}.{{:s}}".format(estResNumber, trialToPlot, neuronToPlot)
+    rocFigFilenamePattern = "../figures/{:08d}_rocAnalysis_trial{:03d}_neuron{:03d}.{{:s}}".format(estResNumber, trialToPlot, neuronToPlot)
+    kernelsParamsFigFilenamePattern = "../figures/{:08d}_trueAndEstimatedKernelsParams.{{:s}}".format(estResNumber)
+    embeddingParamsFigFilenamePattern = "../figures/{:08d}_trueAndEstimatedEmbeddingParams.{{:s}}".format(estResNumber)
+    ksTestTimeRescalingAnalyticalCorrectionFigFilename = "../figures/{:08d}_ksTestTimeRescaling_analyticalCorrection_trial{:03d}_neuron{:03d}.png".format(estResNumber, trialToPlot, neuronToPlot)
+    timeRescalingDiffCDFsFigFilename = "../figures/{:08d}_timeRescalingDiffCDFs_analyticalCorrection_trial{:03d}_neuron{:03d}.png".format(estResNumber, trialToPlot, neuronToPlot)
+    timeRescaling1LagScatterPlotFigFilename = "../figures/{:08d}_timeRescaling1LagScatterPlot_analyticalCorrection_trial{:03d}_neuron{:03d}.png".format(estResNumber, trialToPlot, neuronToPlot)
+    timeRescalingACFFigFilename = "../figures/{:08d}_timeRescalingACF_analyticalCorrection_trial{:03d}_neuron{:03d}.png".format(estResNumber, trialToPlot, neuronToPlot)
 
     estimResConfig = configparser.ConfigParser()
     estimResConfig.read(estimResMetaDataFilename)
     simResNumber = int(estimResConfig["simulation_params"]["simResNumber"])
-    simResConfigFilename = "results/{:08d}_simulation_metaData.ini".format(simResNumber)
+    simResConfigFilename = "../results/{:08d}_simulation_metaData.ini".format(simResNumber)
     simResConfig = configparser.ConfigParser()
     simResConfig.read(simResConfigFilename)
     simInitConfigFilename = simResConfig["simulation_params"]["simInitConfigFilename"]
@@ -70,14 +66,14 @@ def main(argv):
     dtCIF = float(simInitConfig["control_variables"]["dtCIF"])
     trialsLengths = [float(str) for str in simInitConfig["control_variables"]["trialsLengths"][1:-1].split(",")]
     nTrials = len(trialsLengths)
-    tKernels = utils.svGPFA.configUtils.getKernels(nLatents=nLatents, config=simInitConfig, forceUnitScale=True)
+    tKernels = svGPFA.utils.configUtils.getKernels(nLatents=nLatents, config=simInitConfig, forceUnitScale=True)
     kernelsTypes = [type(tKernels[k]).__name__ for k in range(nLatents)]
-    tKernelsParams = utils.svGPFA.initUtils.getKernelsParams0(kernels=tKernels, noiseSTD=0.0)
+    tKernelsParams = svGPFA.utils.initUtils.getKernelsParams0(kernels=tKernels, noiseSTD=0.0)
     simResFilename = simResConfig["simulation_results"]["simResFilename"]
     CFilename = simInitConfig["embedding_params"]["C_filename"]
     dFilename = simInitConfig["embedding_params"]["d_filename"]
-    C, d = utils.svGPFA.configUtils.getLinearEmbeddingParams(CFilename=CFilename, dFilename=dFilename)
-    tIndPointsLocs = utils.svGPFA.configUtils.getIndPointsLocs0(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
+    C, d = svGPFA.utils.configUtils.getLinearEmbeddingParams(CFilename=CFilename, dFilename=dFilename)
+    tIndPointsLocs = svGPFA.utils.initUtils.getIndPointsLocs0(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
     with open(simResFilename, "rb") as f: simRes = pickle.load(f)
     spikesTimes = simRes["spikes"]
     trueLatentsSamples = simRes["latentsSamples"]
@@ -97,18 +93,18 @@ def main(argv):
     model = estResults["model"]
 
     # plot lower bound history
-    fig = plot.svGPFA.plotUtilsPlotly.getPlotLowerBoundHist(lowerBoundHist=lowerBoundHist)
+    fig = svGPFA.plot.plotUtilsPlotly.getPlotLowerBoundHist(lowerBoundHist=lowerBoundHist)
     fig.write_image(lowerBoundHistVsIterNoFigFilenamePattern.format("png"))
     fig.write_html(lowerBoundHistVsIterNoFigFilenamePattern.format("html"))
 
-    fig = plot.svGPFA.plotUtilsPlotly.getPlotLowerBoundHist(elapsedTimeHist=elapsedTimeHist, lowerBoundHist=lowerBoundHist)
+    fig = svGPFA.plot.plotUtilsPlotly.getPlotLowerBoundHist(elapsedTimeHist=elapsedTimeHist, lowerBoundHist=lowerBoundHist)
     fig.write_image(lowerBoundHistVsElapsedTimeFigFilenamePattern.format("png"))
     fig.write_html(lowerBoundHistVsElapsedTimeFigFilenamePattern.format("html"))
 
     # plot true and estimated latents
     testMuK, testVarK = model.predictLatents(times=trueLatentsTimes[0])
     eIndPointsLocs = model.getIndPointsLocs()
-    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedLatents(tTimes=trueLatentsTimes[0], tLatentsSamples=trueLatentsSamples, tLatentsMeans=trueLatentsMeans, tLatentsSTDs=trueLatentsSTDs, tIndPointsLocs=tIndPointsLocs, eTimes=trueLatentsTimes[0], eLatentsMeans=testMuK, eLatentsSTDs=torch.sqrt(testVarK), eIndPointsLocs=eIndPointsLocs, trialToPlot=trialToPlot)
+    fig = svGPFA.plot.plotUtilsPlotly.getPlotTrueAndEstimatedLatents(tTimes=trueLatentsTimes[0], tLatentsSamples=trueLatentsSamples, tLatentsMeans=trueLatentsMeans, tLatentsSTDs=trueLatentsSTDs, tIndPointsLocs=tIndPointsLocs, eTimes=trueLatentsTimes[0], eLatentsMeans=testMuK, eLatentsSTDs=torch.sqrt(testVarK), eIndPointsLocs=eIndPointsLocs, trialToPlot=trialToPlot)
     fig.write_image(latentsFigFilenamePattern.format("png"))
     fig.write_html(latentsFigFilenamePattern.format("html"))
 
@@ -126,12 +122,13 @@ def main(argv):
 
     title = "Trial {:d}, Neuron {:d} ({:d} spikes)".format(trialToPlot, neuronToPlot, len(spikesTimesKS))
 
-    diffECDFsX, diffECDFsY, estECDFx, estECDFy, simECDFx, simECDFy, cb = stats.pointProcess.tests.KSTestTimeRescalingNumericalCorrection(spikesTimes=spikesTimesKS, cifTimes=oneTrialCIFTimes, cifValues=cifValuesKS, gamma=ksTestGamma)
-    plot.svGPFA.plotUtils.plotResKSTestTimeRescalingNumericalCorrection(diffECDFsX=diffECDFsX, diffECDFsY=diffECDFsY, estECDFx=estECDFx, estECDFy=estECDFy, simECDFx=simECDFx, simECDFy=simECDFy, cb=cb, figFilename=ksTestTimeRescalingNumericalCorrectionFigFilename, title=title)
-    plt.close("all")
+    diffECDFsX, diffECDFsY, estECDFx, estECDFy, simECDFx, simECDFy, cb = gcnu_common.stats.point_processes.tests.KSTestTimeRescalingNumericalCorrection(spikesTimes=spikesTimesKS, cifTimes=oneTrialCIFTimes, cifValues=cifValuesKS, gamma=ksTestGamma)
+    fig = svGPFA.plot.plotUtilsPlotly.getPlotResKSTestTimeRescalingNumericalCorrection(diffECDFsX=diffECDFsX, diffECDFsY=diffECDFsY, estECDFx=estECDFx, estECDFy=estECDFy, simECDFx=simECDFx, simECDFy=simECDFy, cb=cb, title=title)
+    fig.write_image(ksTestTimeRescalingNumericalCorrectionFigFilenamePattern.format("png"))
+    fig.write_html(ksTestTimeRescalingNumericalCorrectionFigFilenamePattern.format("html"))
 
     # CIF
-    fig = plot.svGPFA.plotUtilsPlotly.getPlotSimulatedAndEstimatedCIFs(
+    fig = svGPFA.plot.plotUtilsPlotly.getPlotSimulatedAndEstimatedCIFs(
         tTimes=timesTrueValues, 
         tCIF=simCIFsValues[trialToPlot][neuronToPlot], 
         tLabel="True", 
@@ -153,36 +150,37 @@ def main(argv):
                                                        fp=cifValuesKS))
     pk = cifValuesKS_upsampled*dtCIF
     bins = pd.interval_range(start=0, end=int(T), periods=len(pk))
-    cutRes, _ = pd.cut(spikesTimesKS, bins=bins, retbins=True)
+    cutRes, _ = pd.cut(spikesTimesKS.numpy(), bins=bins, retbins=True)
     Y = torch.from_numpy(cutRes.value_counts().values)
     Y[Y>1] = 1
     fpr, tpr, thresholds = sklearn.metrics.roc_curve(Y, pk, pos_label=1)
     roc_auc = sklearn.metrics.auc(fpr, tpr)
-    plot.svGPFA.plotUtils.plotResROCAnalysis(fpr=fpr, tpr=tpr, auc=roc_auc, title=title, figFilename=rocFigFilename)
-    plt.close("all")
+    fig = svGPFA.plot.plotUtilsPlotly.getPlotResROCAnalysis(fpr=fpr, tpr=tpr, auc=roc_auc, title=title)
+    fig.write_image(rocFigFilenamePattern.format("png"))
+    fig.write_html(rocFigFilenamePattern.format("html"))
 
     # plot model params
     mKernelsParams = model.getKernelsParams()
-    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedKernelsParams(
+    fig = svGPFA.plot.plotUtilsPlotly.getPlotTrueAndEstimatedKernelsParams(
         kernelsTypes=kernelsTypes,
         trueKernelsParams=tKernelsParams,
         estimatedKernelsParams=mKernelsParams)
     fig.write_image(kernelsParamsFigFilenamePattern.format("png"))
     fig.write_html(kernelsParamsFigFilenamePattern.format("html"))
 
-    # tLatentsMeansFuncs = utils.svGPFA.configUtils.getLatentsMeansFuncs(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
-    # trialsTimes = utils.svGPFA.miscUtils.getTrialsTimes(trialsLengths=trialsLengths, dt=dtCIF)
-    # tLatentsMeans = utils.svGPFA.miscUtils.getLatentsMeanFuncsSamples(latentsMeansFuncs=tLatentsMeansFuncs, trialsTimes=trialsTimes, dtype=C.dtype)
+    # tLatentsMeansFuncs = svGPFA.utils.configUtils.getLatentsMeansFuncs(nLatents=nLatents, nTrials=nTrials, config=simInitConfig)
+    # trialsTimes = svGPFA.utils.miscUtils.getTrialsTimes(trialsLengths=trialsLengths, dt=dtCIF)
+    # tLatentsMeans = svGPFA.utils.miscUtils.getLatentsMeanFuncsSamples(latentsMeansFuncs=tLatentsMeansFuncs, trialsTimes=trialsTimes, dtype=C.dtype)
     # kernelsParams = model.getKernelsParams()
-    # kernels = utils.svGPFA.configUtils.getKernels(nLatents=nLatents, config=simInitConfig, forceUnitScale=True)
+    # kernels = svGPFA.utils.configUtils.getKernels(nLatents=nLatents, config=simInitConfig, forceUnitScale=True)
     # with torch.no_grad():
     #     latentsMeans, _ = model.predictLatents(newTimes=trialsTimes[0])
-    # fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedKernelsParams(trueKernels=kernels, estimatedKernelsParams=kernelsParams)
+    # fig = svGPFA.plot.plotUtilsPlotly.getPlotTrueAndEstimatedKernelsParams(trueKernels=kernels, estimatedKernelsParams=kernelsParams)
     # fig.write_image(kernelsParamsFigFilenamePattern.format("png"))
     # fig.write_html(kernelsParamsFigFilenamePattern.format("html"))
 
     estimatedC, estimatedD = model.getSVEmbeddingParams()
-    fig = plot.svGPFA.plotUtilsPlotly.getPlotTrueAndEstimatedEmbeddingParams(trueC=C.numpy(), trueD=d.numpy(), estimatedC=estimatedC.numpy(), estimatedD=estimatedD.numpy())
+    fig = svGPFA.plot.plotUtilsPlotly.getPlotTrueAndEstimatedEmbeddingParams(trueC=C.numpy(), trueD=d.numpy(), estimatedC=estimatedC.numpy(), estimatedD=estimatedD.numpy())
     fig.write_image(embeddingParamsFigFilenamePattern.format("png"))
     fig.write_html(embeddingParamsFigFilenamePattern.format("html"))
 
@@ -193,14 +191,14 @@ def main(argv):
 #     utSRISIs, uCDF, cb, utRISIs = stats.pointProcess.tests.KSTestTimeRescalingAnalyticalCorrectionUnbinned(spikesTimes=spikesTimesKS, cifValues=cifValuesKS, t0=t0, tf=tf, dt=dt)
 #     sUTRISIs, _ = torch.sort(utSRISIs)
 
-#     plot.svGPFA.plotUtils.plotResKSTestTimeRescalingAnalyticalCorrection(sUTRISIs=sUTRISIs, uCDF=uCDF, cb=cb, title=title, figFilename=ksTestTimeRescalingAnalyticalCorrectionFigFilename)
+#     svGPFA.plot.plotUtils.plotResKSTestTimeRescalingAnalyticalCorrection(sUTRISIs=sUTRISIs, uCDF=uCDF, cb=cb, title=title, figFilename=ksTestTimeRescalingAnalyticalCorrectionFigFilename)
 #     plt.close("all")
-#     plot.svGPFA.plotUtils.plotDifferenceCDFs(sUTRISIs=sUTRISIs, uCDF=uCDF, cb=cb, figFilename=timeRescalingDiffCDFsFigFilename),
+#     svGPFA.plot.plotUtils.plotDifferenceCDFs(sUTRISIs=sUTRISIs, uCDF=uCDF, cb=cb, figFilename=timeRescalingDiffCDFsFigFilename),
 #     plt.close("all")
-#     plot.svGPFA.plotUtils.plotScatter1Lag(x=utRISIs, title=title, figFilename=timeRescaling1LagScatterPlotFigFilename)
+#     svGPFA.plot.plotUtils.plotScatter1Lag(x=utRISIs, title=title, figFilename=timeRescaling1LagScatterPlotFigFilename)
 #     plt.close("all")
 # #     acfRes, confint = statsmodels.tsa.stattools.acf(x=utRISIs, unbiased=True, alpha=0.05)
-# #     plot.svGPFA.plotUtils.plotACF(acf=acfRes, Fs=1/dt, confint=confint, title=title, figFilename=timeRescalingACFFigFilename),
+# #     svGPFA.plot.plotUtils.plotACF(acf=acfRes, Fs=1/dt, confint=confint, title=title, figFilename=timeRescalingACFFigFilename),
 # #     plt.close("all")
 
     pdb.set_trace()
