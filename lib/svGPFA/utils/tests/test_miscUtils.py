28c6267544114f98e9cf77f817056cc35adaea7c
Joaquin Rapela <joacorapela@gmail.com>
2022-05-07 18:13:41 +0100

    Added

---
+++ lib/svGPFA/utils/tests/test_miscUtils.py
@@ -0,0 +1,54 @@
+
+import sys
+import os
+import pdb
+import math
+from scipy.io import loadmat
+import torch
+sys.path.append("../src")
+import utils.svGPFA.miscUtils
+
+def test_getPropSamplesCovered():
+    N = 100
+    tol = .1
+
+    mean = torch.rand(size=(N,))*2-1
+    std = torch.rand(size=(N,))*0.3
+    sample = torch.normal(mean=mean, std=std)
+    propSamplesCovered = utils.svGPFA.miscUtils.getPropSamplesCovered(sample=sample, mean=mean, std=std, percent=.95)
+    assert(.95-tol<propSamplesCovered and propSamplesCovered<tol+.95)
+
+def test_getDiagIndicesIn3DArray():
+    N = 3
+    M = 2
+    trueDiagIndices = torch.tensor([0, 4, 8, 9, 13, 17])
+
+    diagIndices = utils.svGPFA.miscUtils.getDiagIndicesIn3DArray(N=N, M=M)
+    assert(((trueDiagIndices-diagIndices)**2).sum()==0)
+
+def test_build3DdiagFromDiagVector():
+    N = 3
+    M = 2
+    v = torch.arange(M*N, dtype=torch.double)
+    D = utils.svGPFA.miscUtils.build3DdiagFromDiagVector(v=v, N=N, M=M)
+    trueD = torch.tensor([[[0,0,0],[0,1,0],[0,0,2]],[[3,0,0],[0,4,0],[0,0,5]]], dtype=torch.double)
+    assert(((trueD-D)**2).sum()==0)
+
+# def test_j_cholesky():
+#     tol = 1e-3
+# 
+#     A = torch.randn((3, 4))
+#     K = torch.mm(A, A.T)
+#     trueY = torch.unsqueeze(torch.tensor([1.0, 2.0, 3.0]), 1)
+#     b = torch.mm(K, trueY)
+#     KChol = torch.cholesky(K)
+#     yTorch = torch.cholesky_solve(b, KChol)
+#     yJ = stats.svGPFA.utils.j_cholesky_solve(b, KChol)
+#     error = ((yTorch-yJ)**2).sum()
+#     assert(error<tol)
+# 
+if __name__=="__main__":
+    # test_getDiagIndicesIn3DArray()
+    # test_build3DdiagFromDiagVector()
+    # test_j_cholesky()
+    test_getPropSamplesCovered()
