539189f5b3075a7ff4c18535bac3d5be56fcd336
Joaquin Rapela <joacorapela@gmail.com>
2022-10-16 17:14:49 +0100

    Added header for inclusion on sphinx-gallery

--- examples/scripts/doEstimateSVGPFA.py
+++ examples/scripts/doEstimateSVGPFA.py
@@ -1,3 +1,8 @@
+"""
+Example svGPFA script for parameter estimation -- default parameters
+====================================================================
+
+"""
 import sys
 import os
 import random
