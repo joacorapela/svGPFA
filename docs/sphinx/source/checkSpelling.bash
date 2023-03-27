aedda1505e9ef5f4bf271ec53e107fed5127f36c
Joaquin Rapela <joacorapela@gmail.com>
2023-03-27 19:53:08 +0100

    Deleted

--- docs/sphinx/source/checkSpelling.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rstFiles="highLevelInterface.rst  index.rst introduction.rst lowLevelInterface.rst params.rst references.rst svGPFA.plot.rst svGPFA.rst svGPFA.stats.rst svGPFA.utils.rst"
-
-for rstFile in $rstFiles; do
-    aspell -p ./svGPFA_doc.dic check $rstFile
-done
