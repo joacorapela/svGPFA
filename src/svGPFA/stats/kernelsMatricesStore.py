d29c75ef4b92b68fbf0f3c8e464c86507300459b
Joaquin Rapela <joacorapela@gmail.com>
2023-11-24 16:28:21 +0000

    removed KernelMatricesStoreGettersAndSetters and IndPointsLocsAndAllTimesKMS

--- src/svGPFA/stats/kernelsMatricesStore.py
+++ src/svGPFA/stats/kernelsMatricesStore.py
@@ -39,54 +39,6 @@ class KernelsMatricesStore(abc.ABC):
         return answer
 
 
-class KernelMatricesStoreGettersAndSetters(abc.ABC):
-    def get_flattened_kernels_params(self):
-        flattened_params = []
-        for k in range(len(self._kernels)):
-            flattened_params.extend(self._kernels[k].getParams().flatten().tolist())
-        return flattened_params
-
-    def get_flattened_kernels_params_grad(self):
-        flattened_params_grad = []
-        for k in range(len(self._kernels)):
-            flattened_params_grad.extend(self._kernels[k].getParams().grad.flatten().tolist())
-        return flattened_params_grad
-
-    def set_kernels_params_from_flattened(self, flattened_params):
-        for k in range(len(self._kernels)):
-            kernel_nParams = self._kernels[k].getParams().numel()
-            flattened_param = flattened_params[:kernel_nParams]
-            self._kernels[k].setParams(torch.tensor(flattened_param, dtype=torch.double))
-            flattened_params = flattened_params[kernel_nParams:]
-
-    def set_kernels_params_requires_grad(self, requires_grad):
-        for k in range(len(self._kernels)):
-            self._kernels[k].getParams().requires_grad = requires_grad
-
-    def get_flattened_ind_points_locs(self):
-        flattened_params = []
-        for k in range(len(self._ind_points_locs)):
-            flattened_params.extend(self._ind_points_locs[k].flatten().tolist())
-        return flattened_params
-
-    def get_flattened_ind_points_locs_grad(self):
-        flattened_params_grad = []
-        for k in range(len(self._ind_points_locs)):
-            flattened_params_grad.extend(self._ind_points_locs[k].grad.flatten().tolist())
-        return flattened_params_grad
-
-    def set_ind_points_locs_from_flattened(self, flattened_params):
-        for k in range(len(self._ind_points_locs)):
-            numel = self._ind_points_locs[k].numel()
-            self._ind_points_locs[k] = torch.tensor(flattened_params[:numel],
-                                                    dtype=torch.double).reshape(self._ind_points_locs[k].shape)
-            flattened_params = flattened_params[numel:]
-
-    def set_ind_points_locs_requires_grad(self, requires_grad):
-        for k in range(len(self._ind_points_locs)):
-            self._ind_points_locs[k].requires_grad = requires_grad
-
-
 class IndPointsLocsKMS(KernelsMatricesStore):
 
     def setRegParam(self, reg_param):
@@ -138,11 +90,6 @@ class IndPointsLocsKMS_Chol(IndPointsLocsKMS):
         return solve
 
 
-class IndPointsLocsKMS_CholWithGettersAndSetters(IndPointsLocsKMS_Chol, KernelMatricesStoreGettersAndSetters):
-    def __init__(self):
-        pass
-
-
 class IndPointsLocsKMS_PInv(IndPointsLocsKMS):
 
     def _invertKzz3D(self, Kzz):
@@ -159,12 +106,6 @@ class IndPointsLocsKMS_PInv(IndPointsLocsKMS):
         return solve
 
 
-class IndPointsLocsKMS_PInvWithGettersAndSetters(IndPointsLocsKMS_PInv,
-                                                 KernelMatricesStoreGettersAndSetters):
-    def __init__(self):
-        pass
-
-
 class IndPointsLocsAndTimesKMS(KernelsMatricesStore):
 
     def setTimes(self, times):
@@ -179,31 +120,6 @@ class IndPointsLocsAndTimesKMS(KernelsMatricesStore):
 
     def getKttDiag(self):
         return self._KttDiag
-
-
-class IndPointsLocsAndAllTimesKMS(IndPointsLocsAndTimesKMS):
-
-    def buildKernelsMatrices(self):
-        # self._t \in nTrials x nQuad x 1
-        n_latents = len(self._ind_points_locs)
-        self._Ktz = [[None] for k in range(n_latents)]
-        self._KttDiag = torch.zeros(self._t.shape[0], self._t.shape[1],
-                                    n_latents,
-                                    dtype=self._t.dtype, device=self._t.device)
-        for k in range(n_latents):
-            self._Ktz[k] = self._kernels[k].buildKernelMatrix(X1=self._t, X2=self._ind_points_locs[k])
-            self._KttDiag[:, :, k] = self._kernels[k].buildKernelMatrixDiag(X=self._t).squeeze()
-
-    def buildKttKernelsMatrices(self):
-        # t \in nTrials x nQuad x 1
-        n_latents = len(self._ind_points_locs)
-        self._Ktt = [[None] for k in range(n_latents)]
-
-        for k in range(n_latents):
-            self._Ktt[k] = self._kernels[k].buildKernelMatrix(X1=self._t, X2=self._t)
-
-
-class IndPointsLocsAndAssocTimesKMS(IndPointsLocsAndTimesKMS):
 
     def buildKernelsMatrices(self):
         n_latents = len(self._ind_points_locs)
