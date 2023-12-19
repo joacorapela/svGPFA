b66af8284db5ac13c52c619100ad0f74822052b6
Joaquin Rapela <joacorapela@gmail.com>
2023-12-19 15:01:32 +0000

    Replaced torch by jax

--- src/svGPFA/stats/variationalDist.py
+++ src/svGPFA/stats/variationalDist.py
@@ -2,54 +2,35 @@
 import pdb
 from abc import ABC, abstractmethod
 import math
-import torch
+import jax.numpy as jnp
 
 import svGPFA.utils.miscUtils
 
 class VariationalDist(ABC):
 
-    @abstractmethod
-    def setInitialParams(self, initial_params):
+    def __init__(self):
         pass
 
-    @abstractmethod
-    def getParams(self):
-        pass
+#     @abstractmethod
+#     def setInitialParams(self, initial_params):
+#         pass
 
-    @abstractmethod
-    def getMean(self):
-        pass
+#     @abstractmethod
+#     def getParams(self):
+#         pass
 
-    @abstractmethod
-    def buildCov(self):
-        pass
+#     @abstractmethod
+#     def getMean(self):
+#         pass
+
+#     @abstractmethod
+#     def buildCov(self):
+#         pass
 
 class VariationalDistChol(VariationalDist):
 
     def __init__(self):
         super(VariationalDist, self).__init__()
-
-    def setInitialParams(self, initial_params):
-        nLatents = len(initial_params["mean"])
-        self._mean = [initial_params["mean"][k] for k in range(nLatents)]
-        self._cholVecs = [initial_params["cholVecs"][k] for k in range(nLatents)]
-        self.buildCov()
-
-    def getParams(self):
-        listOfTensors = []
-        listOfTensors.extend([self._mean[k] for k in range(len(self._mean))])
-        listOfTensors.extend([self._cholVecs[k] for k in
-                              range(len(self._cholVecs))])
-        return listOfTensors
-
-    def getMean(self):
-        return self._mean
-
-    def getCov(self):
-        return self._cov
-
-    def buildCov(self):
-        self._cov = svGPFA.utils.miscUtils.buildCovsFromCholVecs(cholVecs=self._cholVecs)
 
 class VariationalDistRank1PlusDiag(VariationalDist):
 
@@ -73,16 +54,17 @@ class VariationalDistRank1PlusDiag(VariationalDist):
         return self._mean
 
     def buildCov(self):
-        R = self._qSVec[0].shape[0]
-        K = len(self._qSVec)
-        qSigma = [[None] for k in range(K)]
-        for k in range(K):
+        n_trials = self._qSVec[0].shape[0]
+        n_latents = len(self._qSVec)
+        qSigma = [[None] for k in range(n_latents)]
+        for k in range(n_latents):
             nIndK = self._qSDiag[k].shape[1]
             # qq \in nTrials x nInd[k] x 1
-            qq = self._qSVec[k].reshape(shape=(R, nIndK, 1))
+            qq = self._qSVec[k].reshape(shape=(n_trials, nIndK, 1))
             # dd \in nTrials x nInd[k] x 1
             nIndKVarRnkK = self._qSVec[k].shape[1]
-            dd = svGPFA.utils.miscUtils.build3DdiagFromDiagVector(v=(self._qSDiag[k].flatten())**2, M=R, N=nIndKVarRnkK)
-            qSigma[k] = torch.matmul(qq, torch.transpose(a=qq, dim0=1, dim1=2)) + dd
+            dd = svGPFA.utils.miscUtils.build3DdiagFromDiagVector(
+                v=(self._qSDiag[k].flatten())**2, M=n_trials, N=nIndKVarRnkK)
+            qSigma[k] = jnp.matmul(qq, jnp.transpose(qq, (0, 2, 1))) + dd
         return(qSigma)
 
