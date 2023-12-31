bdf0cc804f19f0fb2c71eea94d4d3d18929d0663
Joaquin Rapela <joacorapela@gmail.com>
2023-12-31 13:19:48 +0000

    params is now  an array and scale is provided in constructor

--- src/svGPFA/stats/kernels.py
+++ src/svGPFA/stats/kernels.py
@@ -22,80 +22,63 @@ class Kernel(ABC):
 
 class ExponentialQuadraticKernel(Kernel):
 
+    def __init__(self, scale=1.0):
+        self._scale = scale
+
     def buildKernelMatrixX1(self, X1, params):
-        scale = params["scale"]
-        lengthscale = params["lengthscale"]
-        lengthscaleScale = params["lengthscaleScale"]
-        lengthscale = lengthscale/lengthscaleScale
+        lengthscale = params[0]
 
         X2 = X1
         if X1.ndim==3:
             distance = (X1-X2.transpose((0, 2, 1)))**2
         else:
             distance = (X1.reshape(-1,1)-X2.reshape(1,-1))**2
-        covMatrix = scale**2*jnp.exp(-.5*distance/lengthscale**2)
+        covMatrix = self._scale**2*jnp.exp(-.5*distance/lengthscale**2)
         return covMatrix
 
     def buildKernelMatrixX1X2(self, X1, X2, params):
-        scale = params["scale"]
-        lengthscale = params["lengthscale"]
-        lengthscaleScale = params["lengthscaleScale"]
-        lengthscale = lengthscale/lengthscaleScale
+        lengthscale = params[0]
 
         if X1.ndim==3:
             distance = (X1-X2.transpose((0, 2, 1)))**2
         else:
             distance = (X1.reshape(-1,1)-X2.reshape(1,-1))**2
-        covMatrix = scale**2*jnp.exp(-.5*distance/lengthscale**2)
+        covMatrix = self._scale**2*jnp.exp(-.5*distance/lengthscale**2)
         return covMatrix
 
     def buildKernelMatrixDiag(self, X, params):
-        scale = params["scale"]
-        covMatrixDiag = scale**2*jnp.ones(X.shape, dtype=X.dtype)
+        covMatrixDiag = self._scale**2*jnp.ones(X.shape, dtype=X.dtype)
         return covMatrixDiag
 
 
 class PeriodicKernel(Kernel):
 
-    def __init__(self):
-        self.buildKernelMatrixX1_jitted = jax.jit(self.buildKernelMatrixX1)
-        self.buildKernelMatrixX1X2_jitted = jax.jit(self.buildKernelMatrixX1X2)
-        self.buildKernelMatrixDiag_jitted = jax.jit(self.buildKernelMatrixDiag)
+    def __init__(self, scale=1.0):
+        self._scale = scale
 
     def buildKernelMatrixX1(self, X1, params):
-        scale = params["scale"]
-        lengthscale = params["lengthscale"]
-        lengthscaleScale = params["lengthscaleScale"]
-        period = params["period"]
-        periodScale = params["periodScale"]
-        lengthscale = lengthscale / lengthscaleScale
-        period = period / periodScale
+        lengthscale = params[0]
+        period = params[1]
         X2 = X1
         if X1.ndim==3:
             sDistance = X1 - X2.transpose(0, 2, 1)
         else:
             sDistance = X1.reshape(-1,1) - X2.reshape(1,-1)
         rr = math.pi * sDistance / period
-        covMatrix = scale**2 * jnp.exp(-2 * jnp.sin(rr)**2 / lengthscale**2)
+        covMatrix = self._scale**2 * jnp.exp(-2 * jnp.sin(rr)**2 / lengthscale**2)
         return covMatrix
 
     def buildKernelMatrixX1X2(self, X1, X2, params):
-        scale = params["scale"]
-        lengthscale = params["lengthscale"]
-        lengthscaleScale = params["lengthscaleScale"]
-        period = params["period"]
-        periodScale = params["periodScale"]
-        lengthscale = lengthscale / lengthscaleScale
-        period = period / periodScale
+        lengthscale = params[0]
+        period = params[1]
         if X1.ndim==3:
             sDistance = X1 - X2.transpose(0, 2, 1)
         else:
             sDistance = X1.reshape(-1,1) - X2.reshape(1,-1)
         rr = math.pi * sDistance / period
-        covMatrix = scale**2 * jnp.exp(-2 * jnp.sin(rr)**2 / lengthscale**2)
+        covMatrix = self._scale**2 * jnp.exp(-2 * jnp.sin(rr)**2 / lengthscale**2)
         return covMatrix
 
     def buildKernelMatrixDiag(self, X, params):
-        scale = params["scale"]
-        covMatrixDiag = scale**2 * jnp.ones(X.shape, dtype=X.dtype)
+        covMatrixDiag = self._scale**2 * jnp.ones(X.shape, dtype=X.dtype)
         return covMatrixDiag
