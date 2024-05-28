d088080a97c2193627ae8030e9898effb987f651
Joaquin Rapela <joacorapela@gmail.com>
2024-05-28 16:53:43 +0100

    Replaced KtzSpike with KtzSpikes

--- src/svGPFA/stats/svLowerBound.py
+++ src/svGPFA/stats/svLowerBound.py
@@ -4,10 +4,10 @@ from . import expectedLogLikelihood, klDivergence
 
 class SVLowerBound:
 
-    def eval(vMean, vCov, C, d, Kzz, Kzz_inv, KtzQuad, KtzSpike, KttDiag=1.0):
+    def eval(vMean, vCov, C, d, Kzz, Kzz_inv, KtzQuad, KtzSpikes, KttDiag=1.0):
         eLL_value = expectedLogLikelihood.PointProcessELLExpLink.evalSumAcrossTrialsAndNeurons(
             vMean=vMean, vCov=vCov, C=C, d=d, Kzz=Kzz, Kzz_inv=Kzz_inv,
-            KtzQuad=KtzQuad, KtzSpike=KtzSpike, KttDiag=KttDiag)
+            KtzQuad=KtzQuad, KtzSpikes=KtzSpikes, KttDiag=KttDiag)
         kl_sum = klDivergence.KLDivergence.evalSumAcrossLatentsAndTrials(
             vMean=vMean, vCov=vCov, Kzz=Kzz, Kzz_inv=Kzz_inv)
         the_eval = eLL_value-kl_sum
