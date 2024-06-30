6a26217fe7a85776818ffd92b9eaf98181b4eb7a
Joaquin Rapela <joacorapela@gmail.com>
2024-06-30 20:08:41 +0100

    Moved from PyTorch to JAX

--- src/svGPFA/stats/tests/test_klDivergence.py
+++ src/svGPFA/stats/tests/test_klDivergence.py
@@ -17,14 +17,14 @@ def test_evalSumAcrossLatentsTrials():
     dataFilename = os.path.join(os.path.dirname(__file__), "data/Estep_Objective_PointProcess_svGPFA.mat")
 
     mat = loadmat(dataFilename)
-    nLatents = mat['q_sqrt'].shape[1]
-    variational_mean = [jax.device_put(mat['q_mu'][(0,i)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
-    qSVec0 = [jax.device_put(mat['q_sqrt'][(0,i)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
-    qSDiag0 = [jax.device_put(mat['q_diag'][(0,i)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
-    variational_cov = svGPFA.utils.miscUtils.buildRank1PlusDiagCov(
-        vecs=qSVec0, diags=qSDiag0)
+    nLatents = mat['q_sqrt'].shape[0]
+    qMu0list = [jax.device_put(mat['q_mu'][(i, 0)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
+    qSVec0 = [jax.device_put(mat['q_sqrt'][(i, 0)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
+    qSDiag0 = [jax.device_put(mat['q_diag'][(i, 0)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
 
-    ind_points_locs0 = [jax.device_put(mat['Z'][(i,0)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
+    qSigma0list = svGPFA.utils.miscUtils.buildRank1PlusDiagCov(vecs=qSVec0, diags=qSDiag0)
+
+    Z0 = [jax.device_put(mat['Z'][(i,0)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
     matKLDiv = mat['KLd']
     kernelNames = mat["kernelNames"]
     hprs = mat["hprs"]
@@ -33,29 +33,38 @@ def test_evalSumAcrossLatentsTrials():
     kernels_params0 = [[None] for k in range(nLatents)]
     for k in range(nLatents):
         if kernelNames[0,k][0] == "PeriodicKernel":
-            kernels[k] = svGPFA.stats.kernels.PeriodicKernel()
+            kernels[k] = svGPFA.stats.kernels.PeriodicKernel
             kernels_params0[k] = jnp.array([float(hprs[k,0][0].item()),
                                             float(hprs[k,0][1].item())])
         elif kernelNames[0,k][0] == "rbfKernel":
-            kernels[k] = svGPFA.stats.kernels.ExponentialQuadraticKernel()
+            kernels[k] = svGPFA.stats.kernels.ExponentialQuadraticKernel
             kernels_params0[k] = jnp.array([float(hprs[k,0][0].item())])
         else:
             raise ValueError("Invalid kernel name: %s"%(kernelNames[k]))
 
-    indPointsLocsKMS = svGPFA.stats.kernelsMatricesStore.IndPointsLocsKMS_Chol(
-        kernels=kernels)
-    prior_cov, prior_cov_inv = indPointsLocsKMS.buildKernelsMatrices(
-        kernels_params=kernels_params0, ind_points_locs=ind_points_locs0,
+    qMu0 = jnp.empty((len(qMu0list), qMu0list[0].shape[0],
+                      qMu0list[0].shape[1]), dtype=jnp.double)
+    qSigma0 = jnp.empty((len(qSigma0list), qSigma0list[0].shape[0],
+                         qSigma0list[0].shape[1], qSigma0list[0].shape[2]),
+                        dtype=jnp.double)
+    for k in range(len(qMu0list)):
+        qMu0 = qMu0.at[k, :, :].set(qMu0list[k][:, :, 0])
+        qSigma0 = qSigma0.at[k, :, :, :].set(qSigma0list[k][:, :, :])
+
+    Z0array = jnp.empty((len(Z0), Z0[0].shape[0], Z0[0].shape[1],
+                         Z0[0].shape[2]), dtype=jnp.double)
+    for k in range(len(Z0)):
+        Z0array = Z0array.at[k, :, :, :].set(Z0[k])
+
+    indPointsLocsKMS = svGPFA.stats.kernelsMatricesStore.IndPointsLocsKMS_Chol
+    indPointsLocsKMS.init(kernels=kernels)
+    Kzz, Kzz_cho = indPointsLocsKMS.buildKernelsMatrices(
+        kernels_params=kernels_params0, ind_points_locs=Z0array,
         reg_param=reg_param)
 
-    klDiv = svGPFA.stats.klDivergence.KLDivergence(
-        indPointsLocsKMS=indPointsLocsKMS)
+    klDiv = svGPFA.stats.klDivergence.KLDivergence
     klDivEval = klDiv.evalSumAcrossLatentsAndTrials(
-        variational_mean=variational_mean,
-        variational_cov=variational_cov,
-        prior_cov=prior_cov,
-        prior_cov_inv=prior_cov_inv,
-    )
+        vMean=qMu0, vCov=qSigma0, Kzz=Kzz, Kzz_cho=Kzz_cho)
 
     klError = abs(matKLDiv-klDivEval)
 
