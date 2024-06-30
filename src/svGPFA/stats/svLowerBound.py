0e4c19e25e24623296ad38ccda5d497d89e74dd6
Joaquin Rapela <joacorapela@gmail.com>
2024-06-30 18:30:24 +0100

    Replaced Kzz_inv by Kzz_cho

--- src/svGPFA/stats/svLowerBound.py
+++ src/svGPFA/stats/svLowerBound.py
@@ -4,11 +4,11 @@ from . import expectedLogLikelihood, klDivergence
 
 class SVLowerBound:
 
-    def eval(vMean, vCov, C, d, Kzz, Kzz_inv, KtzQuad, KtzSpikes, KttDiag=1.0):
+    def eval(vMean, vCov, C, d, Kzz, Kzz_cho, KtzQuad, KtzSpikes, KttDiag=1.0):
         eLL_value = expectedLogLikelihood.PointProcessELLExpLink.evalSumAcrossTrialsAndNeurons(
-            vMean=vMean, vCov=vCov, C=C, d=d, Kzz=Kzz, Kzz_inv=Kzz_inv,
+            vMean=vMean, vCov=vCov, C=C, d=d, Kzz=Kzz, Kzz_cho=Kzz_cho,
             KtzQuad=KtzQuad, KtzSpikes=KtzSpikes, KttDiag=KttDiag)
         kl_sum = klDivergence.KLDivergence.evalSumAcrossLatentsAndTrials(
-            vMean=vMean, vCov=vCov, Kzz=Kzz, Kzz_inv=Kzz_inv)
+            vMean=vMean, vCov=vCov, Kzz=Kzz, Kzz_cho=Kzz_cho)
         the_eval = eLL_value-kl_sum
         return the_eval
