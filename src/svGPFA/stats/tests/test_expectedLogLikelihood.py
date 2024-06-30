46b2042e119440c2fb1ba78fe1312f454fc7a94f
Joaquin Rapela <joacorapela@gmail.com>
2024-06-30 20:07:00 +0100

    Moved from PyTorch to JAX

--- src/svGPFA/stats/tests/test_expectedLogLikelihood.py
+++ src/svGPFA/stats/tests/test_expectedLogLikelihood.py
@@ -25,13 +25,14 @@ def test_evalSumAcrossTrialsAndNeurons_pointProcessExpLink():
     mat = loadmat(dataFilename)
     nLatents = len(mat['Z'])
     nTrials = mat['Z'][0,0].shape[2]
-    qMu0 = [jax.device_put(mat['q_mu'][(0,i)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
-    qSVec0 = [jax.device_put(mat['q_sqrt'][(0,i)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
-    qSDiag0 = [jax.device_put(mat['q_diag'][(0,i)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
+    qMu0list = [jax.device_put(mat['q_mu'][(i,0)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
+    qSVec0 = [jax.device_put(mat['q_sqrt'][(i,0)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
+    qSDiag0 = [jax.device_put(mat['q_diag'][(i,0)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
     t = jax.device_put(mat['ttQuad'].astype("float64").transpose(2, 0, 1))
     Z0 = [jax.device_put(mat['Z'][(i,0)].astype("float64").transpose(2,0,1)) for i in range(nLatents)]
     C0 = jax.device_put(mat["C"].astype("float64"))
-    b0 = jax.device_put(mat["b"].astype("float64").squeeze())
+    # b0 = jax.device_put(mat["b"].astype("float64").squeeze())
+    b0 = jax.device_put(mat["b"].astype("float64"))
     hermQuadPoints = jax.device_put(mat['xxHerm'].astype("float64"))
     hermQuadWeights = jax.device_put(mat['wwHerm'].astype("float64"))
     legQuadPoints = jax.device_put(mat['ttQuad'].astype("float64").transpose(2, 0, 1))
@@ -40,7 +41,7 @@ def test_evalSumAcrossTrialsAndNeurons_pointProcessExpLink():
     kernelNames = mat["kernelNames"]
     hprs = mat["hprs"]
 
-    qSigma0 = svGPFA.utils.miscUtils.buildRank1PlusDiagCov(vecs=qSVec0,
+    qSigma0List = svGPFA.utils.miscUtils.buildRank1PlusDiagCov(vecs=qSVec0,
                                                            diags=qSDiag0)
     yMat = loadmat(yNonStackedFilename)
     YNonStacked_tmp = yMat['YNonStacked']
@@ -50,59 +51,60 @@ def test_evalSumAcrossTrialsAndNeurons_pointProcessExpLink():
         for n in range(nNeurons):
             YNonStacked[r][n] = jax.device_put(YNonStacked_tmp[r,0][n,0][:,0]).astype("float64")
 
-    YStacked, neuronForSpikeIndex = svGPFA.utils.miscUtils.stackSpikeTimes(spikeTimes=YNonStacked)
-
-    linkFunction = jnp.exp
+    spikes_times_array, valid_spikes_times_mask = \
+        svGPFA.utils.miscUtils.buildSpikesTimesArray(spikes_times=YNonStacked)
 
     kernels = [[None] for k in range(nLatents)]
     kernels_params0 = [[None] for k in range(nLatents)]
     for k in range(nLatents):
         if kernelNames[0,k][0] == "PeriodicKernel":
-            kernels[k] = svGPFA.stats.kernels.PeriodicKernel()
+            kernels[k] = svGPFA.stats.kernels.PeriodicKernel
             lengthscale = float(hprs[k,0][0].item())
             period = float(hprs[k,0][1].item())
             kernels_params0[k] = jnp.array([lengthscale, period])
         elif kernelNames[0,k][0] == "rbfKernel":
-            kernels[k] = svGPFA.stats.kernels.ExponentialQuadraticKernel()
+            kernels[k] = svGPFA.stats.kernels.ExponentialQuadraticKernel
             lengthscale = float(hprs[k,0][0].item())
             kernels_params0[k] = jnp.array([lengthscale])
         else:
             raise ValueError("Invalid kernel name: %s"%(kernelNames[k]))
 
-    eLLCalculationParams = {"leg_quad_points": legQuadPoints,
-                            "leg_quad_weights": legQuadWeights}
+    qMu0 = jnp.empty((len(qMu0list), qMu0list[0].shape[0],
+                      qMu0list[0].shape[1]), dtype=jnp.double)
+    qSigma0 = jnp.empty((len(qSigma0List), qSigma0List[0].shape[0],
+                         qSigma0List[0].shape[1], qSigma0List[0].shape[2]),
+                        dtype=jnp.double)
+    for k in range(len(qMu0list)):
+        qMu0 = qMu0.at[k, :, :].set(qMu0list[k][:, :, 0])
+        qSigma0 = qSigma0.at[k, :, :, :].set(qSigma0List[k][:, :, :])
 
-    qK = svGPFA.stats.posteriorOnLatents.PosteriorOnLatents()
-    qHQuadTimes = svGPFA.stats.preIntensity.LinearPreIntensityQuadTimes(
-        posteriorOnLatents=qK)
-    qHSpikesTimes = svGPFA.stats.preIntensity.LinearPreIntensitySpikesTimes(
-        posteriorOnLatents=qK, neuronForSpikeIndex=neuronForSpikeIndex)
-    eLL = svGPFA.stats.expectedLogLikelihood.PointProcessELLExpLink(
-        preIntensityQuadTimes=qHQuadTimes,
-        preIntensitySpikesTimes=qHSpikesTimes,
-        legQuadWeights=legQuadWeights,
-    )
+    Z0array = jnp.empty((len(Z0), Z0[0].shape[0], Z0[0].shape[1],
+                         Z0[0].shape[2]), dtype=jnp.double)
+    for k in range(len(Z0)):
+        Z0array = Z0array.at[k, :, :, :].set(Z0[k])
 
-    indPointsLocsKMS = svGPFA.stats.kernelsMatricesStore.IndPointsLocsKMS_Chol(
-        kernels=kernels)
-    quadTimesKMS = svGPFA.stats.kernelsMatricesStore.IndPointsLocsAndTimesKMS(
-        kernels=kernels, times=legQuadPoints)
-    spikesTimesKMS = svGPFA.stats.kernelsMatricesStore.IndPointsLocsAndTimesKMS(
-        kernels=kernels, times=YStacked)
-    Kzz, Kzz_inv = indPointsLocsKMS.buildKernelsMatrices(
-        kernels_params=kernels_params0, ind_points_locs=Z0, reg_param=reg_param)
-    Ktz_quad, KttDiag_quad = quadTimesKMS.buildKernelsMatrices(
-        kernels_params=kernels_params0, ind_points_locs=Z0)
-    Ktz_spike, KttDiag_spike = spikesTimesKMS.buildKernelsMatrices(
-        kernels_params=kernels_params0, ind_points_locs=Z0)
-    kernels_matrices = dict(Kzz=Kzz, Kzz_inv=Kzz_inv,
-                            Ktz_quad=Ktz_quad, KttDiag_quad=KttDiag_quad, 
-                            Ktz_spike=Ktz_spike, KttDiag_spike=KttDiag_spike)
+    indPointsLocsKMS = svGPFA.stats.kernelsMatricesStore.IndPointsLocsKMS_Chol
+    indPointsLocsKMS.init(kernels=kernels)
+    quadTimesKMS = svGPFA.stats.kernelsMatricesStore.IndPointsLocsAndQuadTimesKMS
+    quadTimesKMS.init(kernels=kernels, t=legQuadPoints)
+    spikesTimesKMS = svGPFA.stats.kernelsMatricesStore.IndPointsLocsAndSpikesTimesKMS
+    spikesTimesKMS.init(kernels=kernels, t=spikes_times_array)
+    Kzz, Kzz_cho = indPointsLocsKMS.buildKernelsMatrices(
+        kernels_params=kernels_params0, ind_points_locs=Z0array, reg_param=reg_param)
+    Ktz_quad = quadTimesKMS.buildKernelsMatrices(
+        kernels_params=kernels_params0, ind_points_locs=Z0array)
+    Ktz_spikes = spikesTimesKMS.buildKernelsMatrices(
+        kernels_params=kernels_params0, ind_points_locs=Z0array)
 
-    sELL = eLL.evalSumAcrossTrialsAndNeurons(variational_mean=qMu0,
-                                             variational_cov=qSigma0,
+    eLL = svGPFA.stats.expectedLogLikelihood.PointProcessELLExpLink
+    eLL.init(legQuadWeights=legQuadWeights,
+             validSpikesTimesMask=valid_spikes_times_mask)
+    sELL = eLL.evalSumAcrossTrialsAndNeurons(vMean=qMu0, vCov=qSigma0,
                                              C=C0, d=b0,
-                                             kernels_matrices=kernels_matrices)
+                                             Kzz=Kzz, Kzz_cho=Kzz_cho,
+                                             KtzQuad=Ktz_quad,
+                                             KtzSpikes=Ktz_spikes,
+                                             KttDiag=1.0)
 
     sELLerror = abs(sELL-Elik)
 
@@ -110,4 +112,3 @@ def test_evalSumAcrossTrialsAndNeurons_pointProcessExpLink():
 
 if __name__=="__main__":
     test_evalSumAcrossTrialsAndNeurons_pointProcessExpLink()
-    # test_evalSumAcrossTrialsAndNeurons_pointProcessQuad()
