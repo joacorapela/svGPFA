11ef8a371b7f6fa4a2001ece9b01720135cb4477
Joaquin Rapela <joacorapela@gmail.com>
2024-06-30 18:22:22 +0100

    Renamed Kzz_inv by Kzz_cho and removed commented code on LinearPreIntensitySpikes

--- src/svGPFA/stats/preIntensity.py
+++ src/svGPFA/stats/preIntensity.py
@@ -5,36 +5,35 @@ import jax.numpy as jnp
 from . import posteriorOnLatents
 
 
-class LinearPreIntensityQuad:
+class LinearPreIntensity:
 
-    def computeMeans(vMean, C, d, Kzz_inv, Ktz):
+    def computeMeans(vMean, C, d, Kzz_cho, Ktz):
         # vMean \in n_latents x n_trials x n_ind_points
         # C \in n_neurons x n_trials
         # d \in n_neurons x 1
-        # Kzz_inv \in n_latents x n_trials x n_ind_points x n_ind_points
+        # Kzz_cho \in n_latents x n_trials x n_ind_points x n_ind_points
         # Ktz \in n_latents x n_trials x n_quad x n_ind_points
         # return n_trials x n_neurons x n_quad
-        qKMu = posteriorOnLatents.PosteriorOnLatentsQuad.computeMeans(
-            vMean=vMean, Kzz_inv=Kzz_inv, Ktz=Ktz)
-        qHMu = LinearPreIntensityQuad.\
-            _computeMeansGivenPosteriorOnLatentsStats(
-                qKMu=qKMu, C=C, d=d)
+        qKMu = posteriorOnLatents.PosteriorOnLatents.computeMeans(
+            vMean=vMean, Kzz_cho=Kzz_cho, Ktz=Ktz)
+        qHMu = LinearPreIntensity._computeMeansGivenPosteriorOnLatentsStats(
+            qKMu=qKMu, C=C, d=d)
         return qHMu
 
-    def computeVars(vCov, C, Kzz, Kzz_inv, Ktz, KttDiag):
+    def computeVars(vCov, C, Kzz, Kzz_cho, Ktz, KttDiag):
         # vMean \in n_latents x n_trials x n_ind_points
         # vCov \in
         #  n_latents x n_trials x n_ind_points x n_ind_points
         # C \in n_neurons x n_trials
         # d \in n_neurons x 1
         # Kzz \in n_latents x n_trials x n_ind_points x n_ind_points
-        # Kzz_inv \in n_latents x n_trials x n_ind_points x n_ind_points
+        # Kzz_cho \in n_latents x n_trials x n_ind_points x n_ind_points
         # Ktz \in n_latents x n_trials x n_quad x n_ind_points
         # KttDiag \in Reals
         # return n_trials x n_neurons x n_quad
-        qKVar = posteriorOnLatents.PosteriorOnLatentsQuad.computeVars(
-            vCov=vCov, Kzz=Kzz, Kzz_inv=Kzz_inv, Ktz=Ktz, KttDiag=KttDiag)
-        qHVar = LinearPreIntensityQuad._computeVarsGivenPosteriorOnLatentsStats(
+        qKVar = posteriorOnLatents.PosteriorOnLatents.computeVars(
+            vCov=vCov, Kzz=Kzz, Kzz_cho=Kzz_cho, Ktz=Ktz, KttDiag=KttDiag)
+        qHVar = LinearPreIntensity._computeVarsGivenPosteriorOnLatentsStats(
             qKVar=qKVar, C=C)
         return qHVar
 
@@ -76,35 +75,3 @@ class LinearPreIntensityQuad:
         qHVar = posteriorOnVars_vmTrials(qKVar, C)
 
         return qHVar
-
-
-class LinearPreIntensitySpikes:
-
-    def init(neuronForSpikeIndex):
-        LinearPreIntensitySpikes.neuronForSpikeIndex = neuronForSpikeIndex
-
-    def computeMeansAndVars(vMean, vCov, C, d, Kzz, Kzz_inv, Ktz, KttDiag):
-        qKMu, qKVar = \
-            posteriorOnLatents.PosteriorOnLatentsSpikes.computeMeansAndVars(
-                vMean=vMean, vCov=vCov, Kzz=Kzz, Kzz_inv=Kzz_inv, Ktz=Ktz,
-                KttDiag=KttDiag)
-        qHMu, qHVar = LinearPreIntensitySpikes.\
-            _computeMeansAndVarsGivenPosteriorOnLatentsStats(
-                qKMu=qKMu, qKVar=qKVar, C=C, d=d)
-        return qHMu, qHVar
-
-    def _computeMeansAndVarsGivenPosteriorOnLatentsStats(qKMu, qKVar, C, d):
-        # qKMu[r], qKVar[r] \in n_spikes_all_neurons[r] x n_latents
-        # qHMu[r], qHVar[r] \in nSpikesFromAllNeuronsInTrial[r]
-        n_trials = len(LinearPreIntensitySpikes.neuronForSpikeIndex)
-        qHMu = [[None] for tr in range(n_trials)]
-        qHVar = [[None] for tr in range(n_trials)]
-        for r in range(n_trials):
-            qHMu[r] = (jnp.sum(qKMu[r] *
-                               C[LinearPreIntensitySpikes.neuronForSpikeIndex[r], :],
-                               axis=1)
-                       + d[LinearPreIntensitySpikes.neuronForSpikeIndex[r]].squeeze())
-            qHVar[r] = jnp.sum(qKVar[r] *
-                               C[LinearPreIntensitySpikes.neuronForSpikeIndex[r], :]**2,
-                               axis=1)
-        return qHMu, qHVar
