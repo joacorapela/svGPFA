e99c8d4476971bb93bda04534d87699052e63089
Joaquin Rapela <joacorapela@gmail.com>
2024-07-07 17:18:09 +0100

    Removed repeated code

--- src/svGPFA/stats/tests/test_kernels.py
+++ src/svGPFA/stats/tests/test_kernels.py
@@ -10,7 +10,7 @@ import svGPFA.stats.kernels
 jax.config.update("jax_enable_x64", True)
 
 def test_exponentialQuadraticKernel_buildKernelMatrixX1():
-    tol = 4e-6
+    tol = 1e-6
     k = 2
     dataFilename = os.path.join(os.path.dirname(__file__),
                                 "data/BuildKernelMatrices.mat")
@@ -19,15 +19,19 @@ def test_exponentialQuadraticKernel_buildKernelMatrixX1():
 
     Z = mat['Z'][k, 0].astype("float64").transpose((2,0,1))
     leasK = mat['Kzz'][k, 0].astype("float64").transpose((2,0,1))
+    epsilon = mat["epsilon"]
     lengthscale = float(mat['hprs'][k][0][0,0])
     params = jnp.array([lengthscale])
 
     kernel = svGPFA.stats.kernels.ExponentialQuadraticKernel
 
     K = kernel.buildKernelMatrixX1(X1=Z, params=params)
+    for k in range(K.shape[0]):
+        K = K.at[k, :, :].set(
+            K[k, :, :] + epsilon * jnp.diag(jnp.ones(K.shape[1]))
+        )
     error = math.sqrt(((K-leasK)**2).flatten().mean())
     assert(error<tol)
-
 
 def test_exponentialQuadraticKernel_buildKernelMatrixX1X2():
     tol = 1e-6
@@ -51,65 +55,6 @@ def test_exponentialQuadraticKernel_buildKernelMatrixX1X2():
 
 def test_periodicKernel_buildKernelMatrixX1():
     tol = 1e-6
-    k = 2
-    dataFilename = os.path.join(os.path.dirname(__file__),
-                                "data/BuildKernelMatrices.mat")
-
-    mat = loadmat(dataFilename)
-    Z = mat['X1'].astype("float64").transpose(2,0,1)
-    leasK = mat['G'].astype("float64").transpose(2,0,1)
-    lengthscale = float(mat['lengthscale'][0,0])
-    period = float(mat['period'][0,0])
-    params = jnp.array([lengthscale, period])
-
-    kernel = svGPFA.stats.kernels.PeriodicKernel
-
-    K = kernel.buildKernelMatrixX1(X1=Z, params=params)
-    error = math.sqrt(((K-leasK)**2).flatten().mean())
-    assert(error<tol)
-
-def test_periodicKernel_buildKernelMatrixX1X2():
-    tol = 1e-6
-    k = 0
-    dataFilename = os.path.join(os.path.dirname(__file__), "data/BuildKernelMatrices.mat")
-
-    mat = loadmat(dataFilename)
-    assert(mat["kernelNames"][0, k][0] == "PeriodicKernel")
-
-    Z = mat['Z'][k, 0].astype("float64").transpose((2,0,1))
-    leasK = mat['Kzz'][k][0].astype("float64").transpose((2,0,1))
-    lengthscale = float(mat['hprs'][k][0][0,0])
-    params = jnp.array([lengthscale])
-
-    kernel = svGPFA.stats.kernels.ExponentialQuadraticKernel
-
-    K = kernel.buildKernelMatrixX1(X1=Z, params=params)
-    error = math.sqrt(((K-leasK)**2).flatten().mean())
-    assert(error<tol)
-
-
-def test_exponentialQuadraticKernel_buildKernelMatrixX1X2():
-    tol = 1e-6
-    k = 2
-    dataFilename = os.path.join(os.path.dirname(__file__), "data/BuildKernelMatrices.mat")
-
-    mat = loadmat(dataFilename)
-    assert(mat["kernelNames"][0, k][0] == "rbfKernel")
-
-    Z = mat['Z'][k, 0].astype("float64").transpose((2,0,1))
-    tt = mat['tt'].astype("float64").transpose((2,0,1))
-    leasKtz = mat['Ktz'][k, 0].astype("float64").transpose((2,0,1))
-    lengthscale = float(mat['hprs'][k][0][0,0])
-    params = jnp.array([lengthscale])
-
-    kernel = svGPFA.stats.kernels.ExponentialQuadraticKernel
-
-    Ktz = kernel.buildKernelMatrixX1X2(X1=tt, X2=Z, params=params)
-    error = math.sqrt(((Ktz-leasKtz)**2).flatten().mean())
-    assert(error<tol)
-
-def test_periodicKernel_buildKernelMatrixX1():
-    tol = 4e-6
     k = 0
     dataFilename = os.path.join(os.path.dirname(__file__),
                                 "data/BuildKernelMatrices.mat")
@@ -117,6 +62,7 @@ def test_periodicKernel_buildKernelMatrixX1():
     mat = loadmat(dataFilename)
     Z = mat['Z'][k, 0].astype("float64").transpose((2,0,1))
     leasK = mat['Kzz'][k, 0].astype("float64").transpose((2,0,1))
+    epsilon = mat["epsilon"]
     lengthscale = float(mat['hprs'][k][0][0,0])
     period = float(mat['hprs'][k][0][1,0])
     params = jnp.array([lengthscale, period])
@@ -124,6 +70,10 @@ def test_periodicKernel_buildKernelMatrixX1():
     kernel = svGPFA.stats.kernels.PeriodicKernel
 
     K = kernel.buildKernelMatrixX1(X1=Z, params=params)
+    for k in range(K.shape[0]):
+        K = K.at[k, :, :].set(
+            K[k, :, :] + epsilon * jnp.diag(jnp.ones(K.shape[1]))
+        )
     error = math.sqrt(((K-leasK)**2).flatten().mean())
     assert(error<tol)
 
