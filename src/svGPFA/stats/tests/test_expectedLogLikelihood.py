f3ffd20c869780c60db6f435740bbad581020c94
Joaquin Rapela <joacorapela@gmail.com>
2025-11-12 15:49:39 +0000

    Added test_spike_times_array_and_mask and test_jnp_where

--- src/svGPFA/stats/tests/test_expectedLogLikelihood.py
+++ src/svGPFA/stats/tests/test_expectedLogLikelihood.py
@@ -1,6 +1,8 @@
 
 import os
+import pickle
 import scipy.io
+import numpy as np
 import jax
 import jax.numpy as jnp
 import svGPFA.utils.miscUtils
@@ -13,6 +15,65 @@ import svGPFA.stats.expectedLogLikelihood
 
 jax.config.update("jax_enable_x64", True)
 
+def test_spike_times_array_and_mask():
+    dataFilename = os.path.join(os.path.dirname(__file__),
+                                "data/spike_times_data.pickle")
+    with open(dataFilename, "rb") as f:
+        load_res = pickle.load(f)
+    # spikes_times[r][n]: list of spikes times for trial r and neuron n
+    spikes_times = load_res["spikes_times"]
+    # spikes_times_array: float64 array \in n_trials x n_spikes x 1
+    spikes_times_array = load_res["spikes_times_array"]
+    # valid_spikes_times_mask: bollean array \in n_trials x n_neurons x n_spikes
+    valid_spikes_times_mask = load_res["valid_spikes_times_mask"]
+
+    n_trials = valid_spikes_times_mask.shape[0]
+    n_neurons = valid_spikes_times_mask.shape[1]
+    n_spikes = valid_spikes_times_mask.shape[2]
+
+    recovered_spikes_times = []
+    for r in range(n_trials):
+        recovered_spikes_times_r = []
+        for n in range(n_neurons):
+            recovered_spikes_times_rn = \
+                spikes_times_array[r, valid_spikes_times_mask[r, n, :], 0]
+            recovered_spikes_times_r.append(recovered_spikes_times_rn)
+        recovered_spikes_times.append(recovered_spikes_times_r)
+
+    for r in range(n_trials):
+        for n in range(n_neurons):
+            assert(set(spikes_times[r][n].tolist()) == set(recovered_spikes_times[r][n].tolist()))
+
+
+def test_jnp_where(n_trials=2, tol=1e-6):
+    dataFilename = os.path.join(os.path.dirname(__file__),
+                                "data/spike_times_data.pickle")
+    with open(dataFilename, "rb") as f:
+        load_res = pickle.load(f)
+    # valid_spikes_times_mask: bollean array \in n_trials x n_neurons x n_spikes
+    valid_spikes_times_mask = load_res["valid_spikes_times_mask"]
+    valid_spikes_times_mask = valid_spikes_times_mask[:n_trials, :, :]
+
+    n_trials = valid_spikes_times_mask.shape[0]
+    n_neurons = valid_spikes_times_mask.shape[1]
+    n_spikes = valid_spikes_times_mask.shape[2]
+
+    # qHMuSpikes \in n_trials x n_neurons x n_spikes
+    qHMuSpikes = np.random.normal(size=(n_trials, n_neurons, n_spikes))
+    eLogLinkValues = jnp.where(valid_spikes_times_mask, qHMuSpikes, 0.0)
+    sELLTerm2 = jnp.sum(eLogLinkValues)
+
+    sELLTerm2_new = 0.0
+    for r in range(n_trials):
+        print(f"*** Processing trial {r}: sELLTerm2={sELLTerm2}, sELLTerm2_new={sELLTerm2_new}")
+        for n in range(n_neurons):
+            print(f"Processing neuron {n}: sELLTerm2={sELLTerm2}, sELLTerm2_new={sELLTerm2_new}")
+            for i in range(n_spikes):
+                if valid_spikes_times_mask[r, n, i]:
+                    sELLTerm2_new += qHMuSpikes[r, n, i]
+
+    print(f"Processing done: sELLTerm2={sELLTerm2}, sELLTerm2_new={sELLTerm2_new}")
+    assert(abs(sELLTerm2 - sELLTerm2_new) < tol)
 
 def test_evalSumAcrossTrialsAndNeurons_pointProcessExpLink():
     tol = 3e-4
@@ -107,4 +168,6 @@ def test_evalSumAcrossTrialsAndNeurons_pointProcessExpLink():
     assert(sELLerror<tol)
 
 if __name__=="__main__":
-    test_evalSumAcrossTrialsAndNeurons_pointProcessExpLink()
+    # test_evalSumAcrossTrialsAndNeurons_pointProcessExpLink()
+    # test_spike_times_array_and_mask()
+    test_jnp_where()
