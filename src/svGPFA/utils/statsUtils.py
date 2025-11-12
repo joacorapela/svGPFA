5a90b582f080e617db54ebb1efcc3c1799c2c810
Joaquin Rapela <joacorapela@gmail.com>
2025-11-12 15:40:37 +0000

    Added computeCIFmeanAndSTD

--- src/svGPFA/utils/statsUtils.py
+++ src/svGPFA/utils/statsUtils.py
@@ -1,4 +1,4 @@
-
+import jax.numpy as jnp
 import svGPFA.utils.miscUtils
 import svGPFA.stats.kernelsMatricesStore
 import svGPFA.stats.posteriorOnLatents
@@ -22,12 +22,12 @@ def computeLatents(vMean, vChol, kernels_params, ind_points_locs,
     vCov = svGPFA.utils.miscUtils.buildCovsFromCholVecs(vChol)
     l_vars = svGPFA.stats.posteriorOnLatents.PosteriorOnLatents.computeVars(
         vCov=vCov, Kzz=Kzz, Kzz_cho=Kzz_cho, Ktz=Ktz_quad)
-    breakpoint()
     return l_means, l_vars
 
 
 def computePreIntensity(C, d, vMean, vChol, kernels_params, ind_points_locs,
                         kernels_types, leg_quad_points, reg_param):
+    # h_means, h_vars \in n_trials x n_neurons x n_quad
     kernels = svGPFA.utils.miscUtils.buildKernels(
         kernels_types=kernels_types, kernels_params=kernels_params)
     indPointsLocsKMS = svGPFA.stats.kernelsMatricesStore.IndPointsLocsKMS_Chol
@@ -45,3 +45,10 @@ def computePreIntensity(C, d, vMean, vChol, kernels_params, ind_points_locs,
     h_vars = svGPFA.stats.preIntensity.LinearPreIntensity.computeVars(
         vCov=vCov, C=C, Kzz=Kzz, Kzz_cho=Kzz_cho, Ktz=Ktz_quad)
     return h_means, h_vars
+
+def computeCIFmeanAndSTD(h_means, h_vars):
+    # h_means, h_vars \in n_trials x n_neurons x n_quad
+    # answer \in n_trials x n_neurons x n_quad
+    mean = jnp.exp(h_means + 0.5 * h_vars)
+    std = mean*jnp.sqrt(jnp.exp(h_vars)-1)
+    return mean, std
